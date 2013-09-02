eb22cdfd8a4441f6fd32d6992c30333d0b8f124f
Pixelastic <tim@pixelastic.com>
2013-09-02 02:41:27 +0200

    Adding custom alias for fantomas.

---
+++ config/zsh/local/fantomas.zsh
@@ -0,0 +1,10 @@
+# Custom colors for this hostname
+promptColor=(
+	hostname	"133"
+)
+
+# RVM config
+path=($path	$HOME/.rvm/bin)
+if [[ -r $HOME/.rvm/scripts/rvm ]]; then
+  source ~/.rvm/scripts/rvm
+fi
