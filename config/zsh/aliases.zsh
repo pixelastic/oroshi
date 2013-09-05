c3eb6bba3c265981d72f63d00a4af9f509b66a2e
Pixelastic <tim@pixelastic.com>
2013-09-05 10:17:35 +0200

    Update freebox mount alias.

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -161,7 +161,7 @@ function mfs-sansa() { mark-for-sync $* sansa }
 function mfs-sansa-sd() { mark-for-sync $* sansa-sd }
 # }}}
 # Freebox {{{
-alias fbx='sudo mount -t cifs //mafreebox.freebox.fr/Disque\ dur/ /home/tim/local/mnt/freebox -o _netdev,guest,directio,uid=1000,iocharset=utf8,file_mode=0777,dir_mode=0777'
+alias fbx='sudo mount -t cifs //mafreebox.freebox.fr/Disque\ dur/ /home/tim/local/mnt/freebox -o _netdev,guest,uid=1000,iocharset=utf8,file_mode=0777,dir_mode=0777'
 alias ufbx='sudo umount /home/tim/local/mnt/freebox'
 alias cdfbx='cd /home/tim/local/mnt/freebox'
 # }}}
