1b6e7a0ce00e7ef77406c51a8301795fca872984
Pixelastic <tim@pixelastic.com>
2013-11-28 10:43:56 +0100

    Adding charles proxy install script.

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -110,6 +110,8 @@ alias gftp='gui gftp'
 alias lowriter='gui lowriter'
 alias poedit='gui poedit'
 alias vlc='gui vlc'
+alias virtualbox='gui virtualbox'
+alias charles='gui charles'
 # }}}
 
 # Apt-get {{{
