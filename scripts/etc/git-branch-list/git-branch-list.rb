f62c34d719f22a63f5ab6daccbdd8d8202a95e54
Pixelastic <tim@pixelastic.com>
2014-03-10 10:54:01 +0100

    Adding color for remote branches and hashes in git branch list.

--- scripts/etc/git-branch-list/git-branch-list.rb
+++ scripts/etc/git-branch-list/git-branch-list.rb
@@ -3,33 +3,43 @@
 
 
 class GitBranchList
-		@branch_colors = {
-			:develop => 184,
-			:master => 69,
-			:hotfix => 160,
-			:release => 28,
-			:feature => 202,
-			:bugfix => 203
-		}
+    @branch_colors = {
+      :develop => 184,
+      :master => 69,
+      :hotfix => 160,
+      :release => 28,
+      :feature => 202,
+      :bugfix => 203,
+      :remotes => 160
+    }
+    @hash_color = 67
 
-	def self.color_branchname(line)
-		branches=/master|hotfix|release|develop|feature/
-		suffix=/\/?[\w\/\-\.]*/
+  def self.color_branchname(txt)
+    branches=/master|hotfix|release|develop|feature|remotes/
+    suffix=/\/?[\w\/\-\.]*/
 
-		line.gsub!(/((#{branches})#{suffix})/) do |foo|
-			fullname, type = $1, $2
-			type="bugfix" if fullname =~ /^feature\/bugfix/
-			color=@branch_colors[type.to_sym]
-			self.color_text(fullname, color)
-		end
-		return line
-	end
+    # branch names
+    txt.gsub!(/((#{branches})#{suffix})/) do |_|
+      fullname, type = $1, $2
+      type="bugfix" if fullname =~ /^feature\/bugfix/
+      color=@branch_colors[type.to_sym]
+      self.color_text(fullname, color)
+    end
 
-	# Wrap a text in color codes
-	def self.color_text(text, color)
-		color = "%03d" % color
-		return "[38;5;#{color}m#{text}[00m"
-	end
+    # commit hash
+    txt.gsub!(/([a-f0-9]{7})/) do |hash|
+      self.color_text(hash, @hash_color)
+    end
+
+
+    return txt
+  end
+
+  # Wrap a text in color codes
+  def self.color_text(text, color)
+    color = "%03d" % color
+    return "[38;5;#{color}m#{text}[00m"
+  end
 
 
 end
