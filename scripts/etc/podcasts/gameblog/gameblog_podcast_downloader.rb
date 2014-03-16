dffd842ac9ae94cac7d721974ffb80dfacd721e7
Pixelastic <tim@pixelastic.com>
2014-03-16 18:19:41 +0100

    Updating regexp to match some inconsistencies in gameblog title format.

--- scripts/etc/podcasts/gameblog/gameblog_podcast_downloader.rb
+++ scripts/etc/podcasts/gameblog/gameblog_podcast_downloader.rb
@@ -3,25 +3,25 @@ require_relative '../podcast_downloader'
 
 class GameblogPodcastDownloader < PodcastDownloader
 
-	def pre_run
-		@list_url = 'http://www.gameblog.fr/podcasts.xml'
-	end
+  def pre_run
+    @list_url = 'http://www.gameblog.fr/podcasts.xml'
+  end
 
-	def node_to_hash(node)
-			title = node.xpath('./title').text
-			url = node.xpath('./enclosure').attr('url').text
-			date = DateTime.parse(node.xpath('./pubDate').text)
+  def node_to_hash(node)
+    title = node.xpath('./title').text
+    url = node.xpath('./enclosure').attr('url').text
+    date = DateTime.parse(node.xpath('./pubDate').text)
 
-			matches = title.match(/Podcast (N°)?([0-9]*)(.[^\:]*)? : (.*)$/i)
-			index = matches[2]
-			title = matches[4]
+    matches = title.match(/Podcast (N°)?([0-9]*)(.[^\:]*)?: (.*)$/i)
+    index = matches[2]
+    title = matches[4]
 
-			return {
-				'index' => index,
-				'title' => title,
-				'url' => url,
-				'date' => date,
-			}
-	end
+    return {
+      'index' => index,
+      'title' => title,
+      'url' => url,
+      'date' => date,
+    }
+  end
 end
 
