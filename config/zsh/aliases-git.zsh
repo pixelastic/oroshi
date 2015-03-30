64a7fce9959369500e4753253cdd409fec391b8d
Pixelastic <tim@pixelastic.com>
2015-03-30 11:50:12 +0200

    feat(git): Remove last stash when applying it.

--- config/zsh/aliases-git.zsh
+++ config/zsh/aliases-git.zsh
@@ -77,7 +77,7 @@ alias vtt='git current-tag'
 alias vst='git stash -u'
 alias vstR='git stash drop'
 alias vstRa='git stash clear'
-alias vsta='git stash apply'
+alias vsta='git stash-apply'
 alias vstl='git stash list'
 # }}}
 # [s]ub-[m]odules {{{
