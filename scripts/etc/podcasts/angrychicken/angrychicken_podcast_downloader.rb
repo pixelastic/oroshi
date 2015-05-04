21d8574b072b9f6b63c78ed6caaa6d04ae910740
Pixelastic <tim@pixelastic.com>
2015-05-04 11:20:20 +0200

    feat(angrychicken): Adding angrychicken downloader.

---
+++ scripts/etc/podcasts/angrychicken/angrychicken_podcast_downloader.rb
@@ -0,0 +1,27 @@
+# encoding : utf-8
+require_relative '../podcast_downloader'
+
+class AngryChickenPodcastDownloader < PodcastDownloader
+
+  def pre_run
+    @list_url = 'http://feeds.feedburner.com/theangrychicken?format=xml'
+  end
+
+  def node_to_hash(node)
+    title = node.xpath('./title').text
+    url = node.xpath('./enclosure').attr('url').text
+    date = DateTime.parse(node.xpath('./pubDate').text)
+
+    matches = title.match(/#([0-9]*) - (.*)$/i)
+    index = matches[1]
+    title = matches[2]
+
+    return {
+      'index' => index,
+      'title' => title,
+      'url' => url,
+      'date' => date,
+    }
+  end
+end
+
