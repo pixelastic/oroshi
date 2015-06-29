c110547cbb2ae12cf2b5c894a6dd6534c81f86a8
Pixelastic <tim@pixelastic.com>
2015-06-29 12:07:33 +0200

    style(apt): Follow rubocop suggestions

--- scripts/etc/apt-get-helper/apt-get-helper.rb
+++ scripts/etc/apt-get-helper/apt-get-helper.rb
@@ -1,34 +1,34 @@
 module AptGetHelper
   # TODO: installed? retourne true même si le package a été désinstallé
   @@colors = {
-    :error => 160,
-    :package => 141,
-    :version => 241,
-    :success => 35
+    error: 160,
+    package: 141,
+    version: 241,
+    success: 35
   }
 
   # Loading all information on startup
   @@installed_packages = {}
-  %x[dpkg -l | awk '{print $1,$2,$3}'].split("\n").each do |package|
-    type, name, version = package.split(" ")
+  `dpkg -l | awk '{print $1,$2,$3}'`.split("\n").each do |package|
+    type, name, version = package.split(' ')
     # Keep only packages marked as installed
-    next unless type == "ii"
+    next unless type == 'ii'
     @@installed_packages[name] = version
   end
 
   def colorize(text, color)
-    color = "%03d" % @@colors[color]
-    return "[38;5;#{color}m#{text}[00m"
+    color = '%03d' % @@colors[color]
+    "[38;5;#{color}m#{text}[00m"
   end
 
   def longest_by_type(list, type)
-    ordered = list.map {|obj| obj[type] }.group_by(&:size)
+    ordered = list.map { |obj| obj[type] }.group_by(&:size)
     return nil if ordered.size == 0
-    return ordered.max.last[0]
+    ordered.max.last[0]
   end
 
-  def is_installed?(package)
-    @@installed_packages.has_key?(package)
+  def installed?(package)
+    @@installed_packages.key?(package)
   end
 
   def install(package)
@@ -36,9 +36,7 @@ module AptGetHelper
   end
 
   def get_version(package)
-    return nil if !is_installed? package
+    return nil unless installed? package
     @@installed_packages[package]
   end
-
 end
-
