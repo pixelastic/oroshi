5a5ec611cb1c8a555946f827b28be32502eef60d
Pixelastic <tim@pixelastic.com>
2015-08-14 18:38:15 +0200

    Fix zsh start/end of line mappings

--- config/zsh/keybindings.zsh
+++ config/zsh/keybindings.zsh
@@ -4,37 +4,44 @@ stty ixoff -ixon
 
 # Enabling vim mode
 bindkey -v
-
+ 
 # Ctrl-O to go to ~/.oroshi/
 bindkey -s '^O' '^U cdo^M'
 # Ctrl-K to ls
 bindkey -s '^K' '^U ls^M'
-# Ctrl-S to git status
-bindkey -s '^S' '^U vdl^M'
-# Ctrl-B to git branch
-bindkey -s '^B' '^U vbl^M'
-# Ctrl-V to edit the line in Vim
+# Ctrl-J to git status
+bindkey -s '^J' '^U vdl^M'
+# Ctrl-E to edit the line in Vim
 autoload -U edit-command-line
 zle -N edit-command-line
-bindkey '^V' edit-command-line
+bindkey '^E' edit-command-line
 # Ctrl-P for fuzzy finding
 # Use ag for Ctrl-P fuzzy find
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 bindkey '^P' fzf-file-widget
-# Ctrl-J cycle through words on the line
-autoload -Uz copy-earlier-word
-zle -N copy-earlier-word
-bindkey "^J" copy-earlier-word
-
+ 
 # (re)enabling keybindings in insert mode
+# Note: press Ctrl-V followed by a key to see its keycode
 bindkey -M viins "[3~" delete-char        # Delete
-bindkey -M viins "" backward-delete-char  # Backspace
-bindkey -M viins "" backward-kill-word    # Ctrl-w
-bindkey -M viins "OH"  beginning-of-line  # Home
-bindkey -M viins "[5~" beginning-of-line  # Page Up
-bindkey -M viins "OF"  end-of-line        # End
-bindkey -M viins "[6~" end-of-line        # Page Down
+bindkey -M viins ""    backward-delete-char  # Backspace
 
+# Beginning of line
+bindkey -M viins "[1~" beginning-of-line  # Home (in termite)
+bindkey -M viins "OH"  beginning-of-line  # Home (in terminator)
+bindkey -M viins "[5~" beginning-of-line  # Page Up
+bindkey -M vicmd "H"     beginning-of-line
+bindkey -M vicmd "[1~" beginning-of-line  # Home (in termite)
+bindkey -M vicmd "OH"  beginning-of-line  # Home (in terminator)
+bindkey -M vicmd "[5~" beginning-of-line  # Page Up
+# End of line
+bindkey -M viins "[4~" end-of-line        # End (in termite)
+bindkey -M viins "OF"  end-of-line        # End (in terminator)
+bindkey -M viins "[6~" end-of-line        # Page Down
+bindkey -M vicmd "L"     end-of-line
+bindkey -M vicmd "[4~" end-of-line        # End (in termite)
+bindkey -M vicmd "OF"  end-of-line        # End (in terminator)
+bindkey -M vicmd "[6~" end-of-line        # Page Down
+ 
 # Backward search
 bindkey -M viins "" history-incremental-search-backward
 bindkey -M vicmd "" history-incremental-search-backward
@@ -44,12 +51,7 @@ bindkey -M vicmd "[25~" vi-insert
 # As well as the classic Esc and i
 bindkey -M viins "" vi-cmd-mode
 bindkey -M vicmd "i"  vi-insert
-
-# Move on line
-bindkey -M vicmd "" beginning-of-line
-bindkey -M vicmd "" end-of-line
-
-
+ 
 # Vim cursor {{{
 function _cursor-cmd() {
 	print -n '\e]12;#D70000\a'
