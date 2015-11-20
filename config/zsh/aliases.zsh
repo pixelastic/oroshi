67e09c905df1a20e2be508669162c7f6e83d32ec
Pixelastic <tim@pixelastic.com>
2015-11-20 16:01:01 +0100

    feat(music): Allow downloading mp3 from youtube

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -80,6 +80,7 @@ alias p8='ping 8.8.8.8'
 alias pd='plowdown'
 alias td='transmission-download'
 alias yt='youtube-dl -t --prefer-free-format --max-filesize 700m'
+alias ytx='youtube-dl -t -x --audio-format mp3'
 alias gfv="get_flash_videos -y"
 # }}}
 # GUI apps {{{
