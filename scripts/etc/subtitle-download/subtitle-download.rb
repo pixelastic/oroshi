81bdaf9cb450140428343f12170a22bc64753ca0
Pixelastic <tim@pixelastic.com>
2016-08-13 22:59:06 +0200

    fix(subtitle): Fix downloading of subtitles

--- scripts/etc/subtitle-download/subtitle-download.rb
+++ scripts/etc/subtitle-download/subtitle-download.rb
@@ -1,75 +1,71 @@
-# encoding : utf-8
+require 'English'
+require 'awesome_print'
 require 'curb'
 require 'nokogiri'
 
 # Download subtitles from sous-titres.eu
+# Usage:
+#   $ subtitle-download "game of thrones" 6 1
+#   $ subtitle-download "snatch"
 class SubtitleDownload
   def initialize(*args)
-    @website_url = 'https://www.sous-titres.eu/'
+    @is_movie = args.length == 1
+    @is_serie = args.length > 1
     @name = args[0]
-    @season = args[1]
-    @episode = args[2]
-    @movie = !args[1]
+
+    if @is_serie
+      @host = 'https://www.sous-titres.eu/series/'
+      @season = args[1].to_i
+      @episode = args[2].to_i
+    end
+
+    @host = 'https://www.sous-titres.eu/movies/' if @is_movie
   end
 
   def page_url
-    url = @website_url
-    if @movie
-      url += 'movies/'
-    else
-      url += 'series/'
-    end
-    url += @name.gsub(' ', '_').downcase
-    url + '.html'
+    url = @host
+    url += @name.tr(' ', '_').downcase
+    url += '.html'
+    url
   end
 
-  def page_available(url)
-    output = `wget --spider -O /dev/null -q '#{url}' && echo 1 || echo 0`
-    output.strip!
-    return true if output == '1'
-    false
+  def page_available?(url)
+    `wget --spider -q '#{url}'`
+    $CHILD_STATUS.success?
   end
 
-  def get_links_from_url(url)
+  def link(url)
     body = Curl.get(url).body_str
     doc = Nokogiri::HTML(body)
-    all_links = doc.css('a.subList')
-    # keeping only french links
-    links = []
-    all_links.each do |link|
-      links << link['href'] if link.css('span.lang img[title=fr]').length > 0
-    end
-    links
-  end
+    tags = doc.css('a.subList')
 
-  def get_matching_link_from_list(list)
-    list.each do |link|
-      basename = File.basename(link)
-      if @movie
-        return @website_url + 'movies/' + link if basename.match(/(#{@name})/)
-      else
-        if basename.match(/(S?)(0?)(#{@season})(E|x)(0?)(#{@episode})([^0-9])/)
-          return @website_url + 'series/' + link
-        end
-      end
+    tags.each do |tag|
+      lang = tag.css('span.lang img')[0]['title']
+      next if lang != 'fr'
+
+      season, episode = tag.css('.episodenum').text.split('×')
+      next if season.to_i != @season
+      next if episode.to_i != @episode
+
+      return "#{@host}#{tag['href']}"
     end
     nil
   end
 
   def run
-    page_url = page_url
-    unless page_available(page_url)
-      puts "Unable to access #{page_url}"
-      return
+    url = page_url
+
+    unless page_available?(url)
+      puts "Unable to access #{url}"
+      exit 1
     end
 
-    links = get_links_from_url(page_url)
-    matching_link = get_matching_link_from_list(links)
-
-    unless matching_link
-      puts 'Unable to find subtitle'
-      return
+    download_link = link(url)
+    if download_link.nil?
+      puts 'No subtitle found'
+      exit 1
     end
-    `wget -c '#{matching_link}'`
+
+    `wget -c '#{download_link}'`
   end
 end
