77d9923f17f777c9057766eb250c45d9ac032435
Pixelastic <tim@pixelastic.com>
2017-03-14 11:14:45 +0100

    fix(yarn): Add globally installed packaged accessible through path

--- config/zsh/nvm.zsh
+++ config/zsh/nvm.zsh
@@ -2,4 +2,11 @@
 export NVM_DIR="$HOME/.nvm"
 if [[ -s "$NVM_DIR/nvm.sh" ]]; then
   source "$NVM_DIR/nvm.sh"
+  # Installing packages globally with `npm install -g` correctly put them in the
+  # nvm binary folder
+  # Installing the same with yarn will put them all in $(yarn global bin)
+  # https://github.com/yarnpkg/yarn/issues/1321
+  if command -v yarn > /dev/null; then
+    export PATH=$PATH:~/.config/yarn/global/node_modules/.bin
+  fi
 fi
