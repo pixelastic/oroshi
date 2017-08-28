5b649009c22bdd44f915e8c7a312619607cb8abd
Pixelastic <tim@pixelastic.com>
2017-08-28 19:16:41 +0200

    Adding local zsh config for rorohiko

---
+++ config/zsh/local/rorohiko.zsh
@@ -0,0 +1,28 @@
+# Custom colors for this hostname
+promptColor=(
+	hostname	"033"
+)
+
+# Directories {{{
+alias cdbooks='cd ~/Dropbox/backup/books'
+alias cdrp='cd ~/Dropbox/backup/roleplay/'
+alias cdscenar='cd ~/Dropbox/backup/roleplay/scenarios/'
+# }}}
+
+# Oroshi {{{
+# Reload keybings for this OS
+function ok() {
+  ~/.oroshi/scripts/deploy/xmodmap
+  source ~/.oroshi/private/config/ubuntu/rorohiko/keybindings/windows.sh
+  source ~/.oroshi/private/config/ubuntu/rorohiko/keybindings/custom.sh
+  source ~/.oroshi/private/config/ubuntu/rorohiko/keybindings/media.sh
+}
+# }}}
+
+# Dump file to online domain
+function dumptmp() {
+  rsync -Pharz \
+    $1 \
+    pixelastic:./local/www/tmp.pixelastic.com/share
+    echo "Available on http://tmp.pixelastic.com/share/${1:t}"
+}
