9c8998d95e6d355b3b12ce5c97d2809b4153b238
Pixelastic <tim@pixelastic.com>
2017-09-11 11:36:58 +0200

    feat(inbox): Adding shortcut to open Inbox

--- config/autokey/data/Sample Scripts/Inbox.py
+++ config/autokey/data/Sample Scripts/Inbox.py
@@ -1,4 +1,2 @@
-choices = ["konqueror", "firefox", "chrome"]
-retCode, choice = dialog.list_menu(choices)
-if retCode == 0:
-  system.exec_command(choice + " " + clipboard.get_selection())
+# Open Inbox
+system.exec_command("google-chrome --profile-directory=\"Profile 2\" https://inbox.google.com/")
