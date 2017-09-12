fe50107498513a6a636cc90b2cbe514e4bb05883
Pixelastic <tim@pixelastic.com>
2017-09-12 14:50:40 +0200

    feat(jrnl) Add jrnle alias to edit written journal

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -80,6 +80,8 @@ alias extension-list="find . -type f | awk -F'.' '{print \$NF}' | sort| uniq -c 
 alias guard='guard --clear'
 # Check connectivity
 alias p8='ping 8.8.8.8'
+# Edit the journal entries
+alias jrnle='jrnl --edit'
 # }}}
 # Download {{{
 alias pd='plowdown'
