0d67c1c07883d4300c553925d662ea1398e96147
Pixelastic <tim@pixelastic.com>
2018-12-01 23:26:49 +0100

    Add BG:EE aliases

--- config/zsh/local/nova.zsh
+++ config/zsh/local/nova.zsh
@@ -1,7 +1,6 @@
 # Oroshi {{{
 # Reload keybings for this OS
 function ok() {
-  xmodmap ~/.oroshi/config/xmodmap/local/defaults/nova.xmodmap
   ~/.oroshi/scripts/deploy/xmodmap
   ~/.oroshi/scripts/deploy/xbindkeys
   source ~/.oroshi/config/ubuntu/18.04/windows.sh
@@ -16,3 +15,6 @@ function dumptmp() {
     echo "Available on http://tmp.pixelastic.com/share/${1:t}"
 }
 
+alias baldur="okbg && wine \"/home/tim/.local/share/Steam/steamapps/common/Baldur's Gate II Enhanced Edition/baldur.exe\" && okbgu"
+alias okbg="xmodmap ~/.oroshi/config/xmodmap/local/bg.xmodmap"
+alias okbgu="xmodmap ~/.oroshi/config/xmodmap/local/bg-cancel.xmodmap"
