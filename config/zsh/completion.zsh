743bd20990effa3e6ace3252db32853449b1aaeb
Pixelastic <tim@pixelastic.com>
2019-07-24 12:19:47 +0200

    feat(completion): Add *.js completion to yrt

--- config/zsh/completion.zsh
+++ config/zsh/completion.zsh
@@ -4,6 +4,28 @@
 # [ ] Move through suggestions using vim keys
 # [ ] Complete host names in rsync and ssh
 
+# Disable showing the menu on first tab press
+unsetopt MENU_COMPLETE
+# In case of ambiguous match, complete up to the last ambiguous letter...
+setopt LIST_AMBIGUOUS
+# ... and then, pressing TAB will display the menu
+setopt AUTO_MENU
+# Try first to match regexp markers and continue as a normal char if nothing
+# matches.
+unsetopt NOMATCH
+# Completion should also complete .dotfiles
+setopt GLOB_DOTS
+# When COMPLETE_ALIASES is set (default), any alias is considered a new command,
+# so loses all built-in completion for the command it is aliases to. We unset
+# it, so alias g='git' correctly handle git autocompletion
+# The downsite is that if we define a custom alias yrt="yarn run test",
+# completion for yrt will be the same as completion for yarn. To add custom
+# completion, we need to create a new bin (yrt) instead of an alias and define
+# completion for it
+unsetopt COMPLETE_ALIASES
+# Wait 10s before rm ./*
+setopt RM_STAR_WAIT
+
 local zshConfigDir=~/.oroshi/config/zsh
 source $zshConfigDir/completion/yr
 source $zshConfigDir/completion/tmuxinator
@@ -25,8 +47,6 @@ if [ -f '/home/tim/local/src/google-cloud-sdk/completion.zsh.inc' ]; then
   source '/home/tim/local/src/google-cloud-sdk/completion.zsh.inc'; 
 fi
 
-
-
 # Auto escape &, ? and * when needed
 autoload -U url-quote-magic
 zle -N self-insert url-quote-magic
@@ -40,22 +60,6 @@ zstyle ':completion:*' cache-path ~/.zsh/cache
 zstyle ':completion:*' completer _complete _correct
 # Search for completion in the whole filename, not just on the start
 zstyle ':completion:*' matcher-list '' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]}' '+l:|=* r:|=*'
-# Disable showing the menu on first tab press
-unsetopt MENU_COMPLETE
-# In case of ambiguous match, complete up to the last ambiguous letter...
-setopt LIST_AMBIGUOUS
-# ... and then, pressing TAB will display the menu
-setopt AUTO_MENU
-# Try first to match regexp markers and continue as a normal char if nothing
-# matches.
-unsetopt NOMATCH
-# Completion should also complete .dotfiles
-setopt GLOB_DOTS
-# Completion should not handle aliases as new commands, but internally expand
-# them, and suggestion completion for the underlying command
-unsetopt COMPLETE_ALIASES
-# Wait 10s before rm ./*
-setopt RM_STAR_WAIT
 # }}}
 
 # How to display completion... {{{
@@ -75,12 +79,16 @@ setopt LIST_ROWS_FIRST
 # }}}
 
 # Debug method to reload the completion functions {{{
-r() {
-  \rm -f ~/.zcompdump
-  \rm -f ~/.zcompcache/grunt
-  local f
-  f=($zshConfigDir/completion/_*(.))
-  unfunction $f:t 2> /dev/null
-  autoload -U $f:t
+function reloadCompletion() {
+  unfunction $1
+  autoload -U $1
 }
+# r() {
+#   \rm -f ~/.zcompdump
+#   \rm -f ~/.zcompcache/grunt
+#   local f
+#   f=($zshConfigDir/completion/_*(.))
+#   unfunction $f:t 2> /dev/null
+#   autoload -U $f:t
+# }
 # }}}
