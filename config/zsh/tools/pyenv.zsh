3c370ecd58da02e55ddc5f0eb2ac85ea73ece0de
Tim Carry <tim@pixelastic.com>
2020-10-09 11:15:55 +0200

    Update RAM display in tmux

--- config/zsh/tools/pyenv.zsh
+++ config/zsh/tools/pyenv.zsh
@@ -1,11 +1,10 @@
 # Pyenv
 # Switch python version
 [[ $commands[pyenv] ]] || return
-# Make sure pyenv is found by putting it first
-# --no-rehash should make this pretty quick
-# https://github.com/pyenv/pyenv/issues/571
+
+# Init pyenv
 eval "$(pyenv init - --no-rehash)"
 # Do not prefix the current virtual env in the prompt, neither for pyenv nor
 # pipenv
-export PYENV_VIRTUALENV_DISABLE_PROMPT='1'
-export VIRTUAL_ENV_DISABLE_PROMPT='yes'
+# export PYENV_VIRTUALENV_DISABLE_PROMPT='1'
+# export VIRTUAL_ENV_DISABLE_PROMPT='yes'
