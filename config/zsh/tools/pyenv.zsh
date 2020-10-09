315a5ec8513f6d8714885ff40714f5e18df81000
Tim Carry <tim@pixelastic.com>
2020-10-09 11:33:15 +0200

    Update dropbox script

--- config/zsh/tools/pyenv.zsh
+++ config/zsh/tools/pyenv.zsh
@@ -4,7 +4,3 @@
 
 # Init pyenv
 eval "$(pyenv init - --no-rehash)"
-# Do not prefix the current virtual env in the prompt, neither for pyenv nor
-# pipenv
-# export PYENV_VIRTUALENV_DISABLE_PROMPT='1'
-# export VIRTUAL_ENV_DISABLE_PROMPT='yes'
