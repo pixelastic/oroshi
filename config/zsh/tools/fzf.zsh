f7f6fcd919b8e32db0e83a3a30c0fdfe686afbbe
Tim Carry <tim@pixelastic.com>
2020-10-14 17:47:47 +0200

    Add a custom fzf-preview script to display various files

--- config/zsh/tools/fzf.zsh
+++ config/zsh/tools/fzf.zsh
@@ -10,6 +10,6 @@ export FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} --preview-window=down"
 # Command to find files
 export FZF_DEFAULT_COMMAND='fd --type file --hidden --follow --exclude .git'
 # Specific command when opended with Ctrl-P
-export FZF_CTRL_T_OPTS="--preview 'bat --style=numbers --color=always --line-range :500 {}'"
+export FZF_CTRL_T_OPTS="--preview 'fzf-preview {}'"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 # }}}
