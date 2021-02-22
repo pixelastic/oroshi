ea50291fb12c49b59bcb70878c7d6a6342acb983
Tim Carry <tim@pixelastic.com>
2021-02-22 16:05:52 +0100

    Lazyload nvm

--- config/zsh/tools/nvm.zsh
+++ config/zsh/tools/nvm.zsh
@@ -6,7 +6,18 @@
 local defaultNodeVersion="$(<~/.nvm/alias/default)"
 export PATH="$HOME/.nvm/versions/node/v${defaultNodeVersion}/bin:$PATH"
 
-# This enables lazyloading of nvm. It initially creates an alias. When the alias
-# is called it unregister itself, load nvm and go through with the command. Next
-# nvm calls will use the real nvm
-alias nvm="unalias nvm; source ~/.nvm/nvm.sh; nvm $@"
+# We don't load nvm on startup as it takes some time. 
+# Instead, we define an alias that will load it and then execute it. 
+# On first load, it will delete the alias, leaving only the real nvm for future
+# commands
+alias nvm="lazyloadNvm && nvm"
+function lazyloadNvm {
+  # If nvm is a function, and not an alias, it means it has been correctly
+  # loaded. So we do nothing.  This can happen when using the alias inside
+  # a function because aliases are expanded in the function body
+  [[ ! $aliases[nvm] ]] && return;
+
+  echo "Lazyloading nvm"
+  unalias nvm;
+  source ~/.nvm/nvm.sh
+}
