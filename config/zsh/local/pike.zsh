80be6ffde05ba2c27034d8b6e5c5b65c790799dd
Tim Carry <tim@pixelastic.com>
2021-04-29 14:34:40 +0200

    Update keybindings

---
+++ config/zsh/local/pike.zsh
@@ -0,0 +1,6 @@
+function ok() {
+  ~/.oroshi/scripts/deploy/xmodmap # Custom keys
+  ~/.oroshi/config/ubuntu/20.04/keybindings/index.zsh # App bindings
+  ~/.oroshi/scripts/deploy/xbindkeys # Screen and sound
+}
+
