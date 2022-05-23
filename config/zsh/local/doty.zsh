e18e59f44cd2faeee351cd75d3e4e4f2727accb6
Tim Carry <tim@pixelastic.com>
2022-05-23 16:42:37 +0200

    Revert font size on Kitty to 13, in X

---
+++ config/zsh/local/doty.zsh
@@ -0,0 +1,7 @@
+# Reload keybindings
+function ok() {
+  # Reload keybindings
+  ~/.oroshi/scripts/deploy/xmodmap # Custom keys
+  # ~/.oroshi/config/ubuntu/18.04/keybindings.sh # App bindings
+  # ~/.oroshi/scripts/deploy/xbindkeys # Screen and sound
+}
