1ca1815562f0197fa8a39bd1feb7aaa4deb7bb55
Tim Carry <tim@pixelastic.com>
2022-05-24 18:01:22 +0200

    Make remark-fix work with latest node

--- config/remark/remarkrc.fix.js
+++ config/remark/remarkrc.fix.js
@@ -1,8 +1,10 @@
 /* eslint-disable import/no-commonjs */
 const os = require('os');
+const home = os.homedir();
+
 module.exports = {
   plugins: [
-    `${os.homedir()}/.oroshi/node_modules/remark-frontmatter`,
-    `${os.homedir()}/.oroshi/node_modules/remark-reference-links`,
+    `${home}/.oroshi/node_modules/remark-frontmatter/index.js`,
+    `${home}/.oroshi/node_modules/remark-reference-links/index.js`,
   ],
 };
