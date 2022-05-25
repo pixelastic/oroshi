2b8e905915022e657f385ae46052da6f58c684c7
Tim Carry <tim@pixelastic.com>
2022-05-25 11:21:26 +0200

    Update eslint local definitions

--- config/eslint/eslintrc.js
+++ config/eslint/eslintrc.js
@@ -1,6 +1,4 @@
-/* eslint-disable import/no-commonjs */
 module.exports = {
-  extends: 'algolia',
   rules: {
     'no-console': ['error', { allow: ['warn', 'error'] }],
     'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
