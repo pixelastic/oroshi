524ee1847729315e0439482de9053e62cc37f3bb
Tim Carry <tim@pixelastic.com>
2022-06-01 11:07:18 +0200

    updated ok

--- config/zsh/local/doty.zsh
+++ config/zsh/local/doty.zsh
@@ -2,6 +2,5 @@
 function ok() {
   # Reload keybindings
   ~/.oroshi/scripts/deploy/xmodmap # Custom keys
-  ~/.oroshi/config/ubuntu/22.04/keybindings/index.zsh # App bindings
-  # ~/.oroshi/scripts/deploy/xbindkeys # Screen and sound
+  ~/.oroshi/config/ubuntu/22.04/keybindings/index # App bindings
 }
