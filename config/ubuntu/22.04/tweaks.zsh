731f9518184e17e6007ca27faf0ad49b9729f8e2
Tim Carry <tim@pixelastic.com>
2022-11-08 17:11:09 +0100

    Fixes

--- config/ubuntu/22.04/tweaks.zsh
+++ config/ubuntu/22.04/tweaks.zsh
@@ -2,6 +2,12 @@
 
 # Run `dconf-watch` to see in real time which settings are changed when you
 # tinker in the settings UI
+#
+# Note: If keybindings or config does not work, don't forget to click on the
+# gear icon when logging in and picking X11 and not Wayland.
 
 # Disable animations
 dconf write "/org/gnome/desktop/interface/enable-animations" false
+
+# Remove delay before repeating keys
+dconf write "/org/gnome/desktop/peripherals/keyboard/delay" "uint32 150"
