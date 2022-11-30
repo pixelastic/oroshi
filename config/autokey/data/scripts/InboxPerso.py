e69d5db456e3a63a028b6be0395b3b5845ec964e
Tim Carry <tim@pixelastic.com>
2022-11-30 09:58:32 +0100

    Update AutoKey aliases

--- config/autokey/data/scripts/InboxPerso.py
+++
@@ -1,2 +0,0 @@
-# Open Inbox
-system.exec_command("google-chrome --profile-directory=\"Profile 1\" https://inbox.google.com/")
--- config/autokey/data/scripts/InboxPro.py
+++
@@ -1,2 +0,0 @@
-# Open Inbox
-system.exec_command("google-chrome --profile-directory=\"Profile 2\" https://inbox.google.com/")
