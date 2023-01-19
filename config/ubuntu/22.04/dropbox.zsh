7aed85a6115f0e6fdb9f3b4ce446e1ed5aeecfee
Tim Carry <tim@pixelastic.com>
2023-01-19 22:45:00 +0100

    Making Dropbox overlay icons smaller

--- config/ubuntu/22.04/dropbox.zsh
+++ config/ubuntu/22.04/dropbox.zsh
@@ -1,31 +1,18 @@
 #!/usr/bin/env zsh
-# Nautilus overlays are way too big. We'll reduce them by using the 8x8 icon for
-# all resolutions
+# Nautilus overlays are way too big. We'll reduce them by using a custom icon
+# instead
 
 local iconPath="/usr/share/icons/Yaru"
-local -a allSizes
-allSizes=("8x8" "16x16" "24x24" "32x32" "48x48" "256x256")
+local allSizes=("24x24" "32x32" "48x48" "256x256")
 
-# Backup all files on first run
-local backupPath="${iconPath}/__backup__/"
-if [[ ! -d $backupPath ]]; then
-  echo "First run of the script, backuping data"
-  sudo mkdir -p $backupPath
+# Overwrite the icons with our modified one
+local sourcePath="/home/tim/.oroshi/config/ubuntu/22.04/icons/emblem-default.png"
+for size in $allSizes; do
+  local thisPath="${iconPath}/${size}/emblems/emblem-default.png"
+  local thisPath2x="${iconPath}/${size}@2x/emblems/emblem-default.png"
 
-  for i in {1..$#allSizes}; do 
-    local thisSize=${allSizes[$i]}
-    sudo cp -r ${iconPath}/$size ${backupPath}
-    sudo cp -r ${iconPath}/${size}@2x ${backupPath}
-  done
-fi
-
-# Now delete all uptodate emblems
-for i in {1..$#allSizes}; do 
-  local thisSize=${allSizes[$i]}
-  local thisPath="${iconPath}/${thisSize}/emblems/emblem-dropbox-uptodate.png"
-  local thisPath2x="${iconPath}/${thisSize}@2x/emblems/emblem-dropbox-uptodate.png"
-
-  sudo rm -f $thisPath $thisPath2x
+  sudo cp -f $sourcePath $thisPath
+  sudo cp -f $sourcePath $thisPath2x
 done
 
 
