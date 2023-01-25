e2998f375d36c9e85447ca6b0955a091cbe6821e
Tim Carry <tim@pixelastic.com>
2023-01-25 22:57:40 +0100

    Fix coloring of current selection

--- config/zsh/tools/fzf.zsh
+++ config/zsh/tools/fzf.zsh
@@ -11,7 +11,7 @@ function () {
   fzfColors+=("prompt:$COLOR_ALIAS_UI") # > before query
   fzfColors+=("pointer:$COLOR_ALIAS_UI") # > before selected result
   fzfColors+=("bg+:$COLOR_ALIAS_SELECTED_BACKGROUND") # selected result background
-  fzfColors+=("fg+:$COLOR_BLACK") # selected result background
+  fzfColors+=("fg+:$COLOR_ALIAS_SELECTED_FOREGROUND") # selected result foreground
   fzfColors+=("hl:reverse:$COLOR_ALIAS_MATCH") # match
   fzfColors+=("hl+:reverse:$COLOR_ALIAS_MATCH") # match in current line
   fzfColors+=("gutter:-1") # suggestion gutter
