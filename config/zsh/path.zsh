5edd61ae3c6515f294f41c13ae6cc4397916baed
Tim Carry <tim@pixelastic.com>
2023-05-12 17:54:25 +0200

    Auto-load ./script/bin subfolders

--- config/zsh/path.zsh
+++ config/zsh/path.zsh
@@ -2,6 +2,14 @@
 function oroshi_path() {
 	local hostname="$(hostname)"
 
+	# Build the list of subdirs in ./scripts/bin
+	local localBinariesPath=()
+	for directory in ~/.oroshi/scripts/bin/**/; do
+		# Skip all directories starting with __
+		[[ $directory == */__* ]] && continue
+		localBinariesPath+=(${directory:0:-1})
+	done
+
 	local customPath=(
 		# Language binaries
 		~/.yarn/bin
@@ -18,41 +26,14 @@ function oroshi_path() {
 		~/local/src/fzf/bin
 
 		# Custom binaries
-		# TODO: Load those automatically based on the directory structure
-		~/.oroshi/scripts/bin
-		~/.oroshi/scripts/bin/ai
-		~/.oroshi/scripts/bin/convert/bin
-		~/.oroshi/scripts/bin/coriolis/bin
-		~/.oroshi/scripts/bin/docker
-		~/.oroshi/scripts/bin/http
-		~/.oroshi/scripts/bin/img
-		~/.oroshi/scripts/bin/json
-		~/.oroshi/scripts/bin/pdf/bin
-		~/.oroshi/scripts/bin/spotify
-		~/.oroshi/scripts/bin/tmux
-		~/.oroshi/scripts/bin/video/bin
-		~/.oroshi/scripts/bin/zsh
+		$localBinariesPath
 
 		# Private binaries
 		~/.oroshi/private/scripts/bin
 
 		# Local binaries
-		~/.oroshi/scripts/bin/local/$hostname
 		~/.oroshi/private/scripts/bin/local/$hostname
 	)
-
-	# Custom git-related binaries
-	for directory in ~/.oroshi/scripts/bin/git/*; do
-		customPath+=($directory)
-	done
-	# Custom fzf-related binaries
-	for directory in ~/.oroshi/scripts/bin/fzf/**/; do
-		customPath+=($directory)
-	done
-	# Custom img-related binaries
-	for directory in ~/.oroshi/scripts/bin/img/*; do
-		customPath+=($directory)
-	done
 
 	# Prepend our path to the existing path
 	path=($customPath $path)
