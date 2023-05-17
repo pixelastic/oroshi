3132a93365a428ef970d5b8fbda7cba3adfc8634
Tim Carry <tim@pixelastic.com>
2023-05-17 20:43:58 +0200

    Also add yarn-link-remove-all as part of the scripts to lazyloadnvm

--- config/zsh/tools/nvm.zsh
+++ config/zsh/tools/nvm.zsh
@@ -25,7 +25,15 @@ export OROSHI_NVM_LOADED="0"
 
 # Add aliases for all command that would need node, so nvm loads it on first
 # invocation
-export OROSHI_NVM_LAZYLOAD_ALIASES=(node npm nvm yarn yarn-run yarn-link-remove)
+export OROSHI_NVM_LAZYLOAD_ALIASES=(
+	node
+	npm
+	nvm
+	yarn
+	yarn-link-remove
+	yarn-link-remove-all
+	yarn-run
+)
 for command in $OROSHI_NVM_LAZYLOAD_ALIASES; do
 	alias $command="lazyloadNvm $command"
 done
