48b72d5af36be8b6589603f7563c514c09958372
Tim Carry <tim@pixelastic.com>
2023-07-05 11:49:20 +0200

    Some updates to install scripts

--- config/zsh/tools/rust.zsh
+++ config/zsh/tools/rust.zsh
@@ -1,6 +1,10 @@
 # Rust
 function oroshi_tools_rust() {
-	. "$HOME/.cargo/env"
+	# Stop if cargo not installed
+	local cargoPath=~/.cargo/env
+	[[ -r $cargoPath ]] || return
+
+	source $cargoPath
 }
 oroshi_tools_rust
 unfunction oroshi_tools_rust
