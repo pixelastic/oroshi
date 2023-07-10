61c22205b4d44e19f60336df776c0b7824beda53
Tim Carry <tim@pixelastic.com>
2023-07-10 15:58:24 +0200

    Update extensions in Ubuntu list

--- config/ubuntu/22.04/extensions.zsh
+++ config/ubuntu/22.04/extensions.zsh
@@ -1,15 +1,15 @@
 #!/usr/bin/env zsh
 
 echo "Install ExtensionManager"
-flatpak install flathub com.mattjakeman.ExtensionManager
+sudo apt-get install \
+	gnome-shell-extensions \
+	gnome-shell-extension-manager
 
-echo "Install the following extensions:"
+echo "Install the following extensions from the extensions.gnome.org website:"
+echo "You might need to install the dedicated Chrome extensions as advertised on GNOME website"
 echo "- EasyScreenCast"
 echo "    => Record screen video and webcam"
 echo "- Frippery Move Clock"
 echo "    => Move clock on the right of the topbar"
 echo "- Unite"
 echo "    => Merge the window top bar with the screen topbar"
-
-cdo
-g extenstion-man
