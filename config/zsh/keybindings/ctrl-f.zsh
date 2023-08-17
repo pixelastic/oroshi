9ea3ca7c11a2ddd3bd9daf7b70e9f89d4a4f5100
Tim Carry <tim@pixelastic.com>
2023-08-17 14:37:25 +0200

    Fix opening several files at specific lines with ctrl-F

--- config/zsh/keybindings/ctrl-f.zsh
+++ config/zsh/keybindings/ctrl-f.zsh
@@ -1,19 +1,30 @@
 # Ctrl-F search into files in current directory
 oroshi-fzf-regexp-subdir-widget() {
-  export PROMPT_PREVENT_REFRESH="1"
-  local selection="$(fzf-regexp-subdir)"
-  export PROMPT_PREVENT_REFRESH="0"
+	export PROMPT_PREVENT_REFRESH="1"
+	local selection="$(fzf-regexp-subdir)"
+	export PROMPT_PREVENT_REFRESH="0"
 
-  # Stop if no selection is made
-  if [[ "$selection" == "" ]]; then
-    return 1
-  fi
+	# Stop if no selection is made
+	if [[ "$selection" == "" ]]; then
+		return 1
+	fi
 
-  echo $selection
+	# nvim has no way to open multiple files, each at a different line. The hack
+	# is to open vim, then chain vim commands to open the files files at their
+	# respective lines
+	local nvimArguments=()
+	for item in ${=selection}; do
+    local split=(${(@s/:/)item})
+		local filepath=$split[1]
+		local lineNumber=$split[2]
+		nvimArguments+=(-c "tabedit $filepath | $lineNumber")
+	done
+	# Close the first tab (empty file)
+	nvimArguments+=(-c 'tabfirst | quit!')
 
-  nvim -p ${=selection}
+	nvim $nvimArguments
 
-  return 0
+	return 0
 }
 zle -N oroshi-fzf-regexp-subdir-widget
 bindkey '^F' oroshi-fzf-regexp-subdir-widget
