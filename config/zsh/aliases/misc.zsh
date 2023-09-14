edd31c649cab15c8d64540962652576911c7436c
Tim Carry <tim@pixelastic.com>
2023-09-14 16:54:16 +0200

    Remove htop alias

--- config/zsh/aliases/misc.zsh
+++ config/zsh/aliases/misc.zsh
@@ -15,4 +15,4 @@ alias vlc='gui vlc'
 alias flushdns="/etc/init.d/dns-clean start"
 alias htmlmin="html-minifier --remove-comments --collapse-whitespace --remove-attribute-quotes --remove-redundant-attributes --use-short-doctype"
 alias speedtest='wget -O /dev/null http://speedtest.wdc01.softlayer.com/downloads/test500.zip'
-alias htop="htop --delay=10000"
+# alias htop="htop --delay=10000"
