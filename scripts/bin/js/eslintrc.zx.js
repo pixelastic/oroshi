db1b7a7038e927e64ad4d96c95f4b40bb79643ea
Tim Carry <tim@pixelastic.com>
2023-09-14 16:49:02 +0200

    Allow importing golgoth/firost from zx

--- scripts/bin/js/eslintrc.zx.js
+++ scripts/bin/js/eslintrc.zx.js
@@ -1,5 +1,9 @@
 module.exports = {
   extends: ['/home/tim/.oroshi/node_modules/aberlaas/configs/eslint.js'],
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
   globals: {
     chalk: false,
     fs: false,
@@ -8,6 +12,10 @@ module.exports = {
     yaml: false,
   },
   rules: {
+    // Don't remove the #!/usr/bin/env zx at the beginning
     'node/shebang': 0,
+    // Don't choke on import statements
+    'node/no-unsupported-features/es-syntax': 0,
+    'node/no-unpublished-import': 0,
   },
 };
