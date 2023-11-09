e73b0aece271cdb1fb9339be6baaae1d70c5acfb
Tim Carry <tim@pixelastic.com>
2023-11-09 22:41:52 +0100

    Cleanup zx and mjs files

--- scripts/bin/js/eslintrc.zx.js
+++ scripts/bin/js/eslintrc.zx.js
@@ -4,7 +4,22 @@ module.exports = {
     ecmaVersion: 11,
     sourceType: 'module',
   },
+  // Define all the API reference
+  // https://google.github.io/zx/api
   globals: {
+    $: false,
+    cd: false,
+    fetch: false,
+    question: false,
+    sleep: false,
+    echo: false,
+    stdin: false,
+    within: false,
+    retry: false,
+    spinner: false,
+    glob: false,
+    which: false,
+    argv: false,
     chalk: false,
     fs: false,
     os: false,
@@ -17,5 +32,8 @@ module.exports = {
     // Don't choke on import statements
     'node/no-unsupported-features/es-syntax': 0,
     'node/no-unpublished-import': 0,
+    'node/no-extraneous-import': 0,
+    // Allow simply defining the exit codes
+    'no-process-exit': 0,
   },
 };
