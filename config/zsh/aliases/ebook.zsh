2c503e3d62540d57328fddec8b83be8dc72d485f
Tim Carry <tim@pixelastic.com>
2024-01-26 15:15:39 +0100

    Update methods to interact with ebooks

--- config/zsh/aliases/ebook.zsh
+++ config/zsh/aliases/ebook.zsh
@@ -1,6 +1,14 @@
 # Ebook
 alias ebook-viewer='gui ebook-viewer'
 alias ec='ebook-convert'
-alias emu='ebook-metadata-update'
-alias em='ebook-meta'
+alias ea='ebook-add'
 alias ev='ebook-viewer'
+
+# Metadata
+alias em='ebook-meta'
+alias emu='ebook-metadata-update'
+
+# Cover
+alias ecR="ebook-cover-remove"
+alias ecu="ebook-cover-update"
+alias ec.="ebook-cover-current"
--- config/zsh/aliases/images.zsh
+++ config/zsh/aliases/images.zsh
@@ -3,3 +3,9 @@ alias eog='image-open'
 alias pinta='gui pinta'
 alias dim="dimensions"
 alias dimn="dimensions"
+
+# JPG
+alias ijc="image-jpg-create"
+alias igc="image-gif-create"
+alias ipc="image-png-create"
+alias isc="image-svg-create"
