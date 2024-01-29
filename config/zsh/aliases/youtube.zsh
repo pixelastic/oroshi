157ddfca2fb83f2131396dd41fefe5ceea81c8d6
Tim Carry <tim@pixelastic.com>
2024-01-29 17:15:45 +0100

    Add youtube-* scripts

---
+++ config/zsh/aliases/youtube.zsh
@@ -0,0 +1,19 @@
+# Downloading videos
+alias youtube-dl="yt-dlp"
+
+# Video
+alias yt='youtube-video-download'
+alias ytv='youtube-video-download'
+alias ytvd='youtube-video-download'
+
+# Audio
+alias ytx='youtube-audio-download'
+alias yta='youtube-audio-download'
+alias ytad='youtube-audio-download'
+
+# Playlist
+alias ytp="youtube-playlist-download"
+alias ytpd="youtube-playlist-download"
+alias ytpllr="youtube-playlist-list-raw"
+alias ytpll="youtube-playlist-list"
+alias ytpld="youtube-playlist-download"
