5fab4f0e0e97c7b711715b6938e692a4115a02ec
Tim Carry <tim@pixelastic.com>
2024-04-15 11:25:39 +0200

    Update cheatsheet zsh

--- config/nvim/cheats/zsh/is-in-array.zsh
+++ config/nvim/cheats/zsh/is-in-array.zsh
@@ -1,8 +1,9 @@
 # To check if an element is in an array, we need to get its index with (ie) and
 # make sure the result is not more than the size of the array
 #
-if [[ ! ${myArray[(ie)${myVar}]} -gt ${#myArray} ]]; then
+if [[ ! ${myArray[(ie)${myVar}]} -le ${#myArray} ]]; then
   # It's in the array
+  true
 fi
 
 # For large arrays, it's better to build an inverted list
