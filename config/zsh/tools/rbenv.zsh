80a61ae808224a7c9b8f568c2c154f67fe0ffef2
Tim Carry <tim@pixelastic.com>
2024-07-07 18:03:55 +0200

    Add RUBY_CONFIGURE_OPTS

--- config/zsh/tools/rbenv.zsh
+++ config/zsh/tools/rbenv.zsh
@@ -1,11 +1,14 @@
 # Rbenv
-# This file is empty on purpose.
 # The suggested way of installing rbenv is running eval "$(rbenv init -)"
 # This adds ~150ms to shell startup. The only thing important it's doing is
 # running rbenv rehash, which is what takes the most time.
 #
 # Rehashing is needed to update the shims whenever we add new gems, so I added
-# some wrapped scripts (bundle-install/update and gem-install/update) that will
+# some wrapper scripts (bundle-install/update and gem-install/update) that will
 # call the rehash.
 #
 # Source: https://github.com/rbenv/rbenv#how-rbenv-hooks-into-your-shell
+
+# We disable the installation of local docs when installing gems, as it adds
+# time to the install for a feature I never use.
+RUBY_CONFIGURE_OPTS=--disable-install-doc
