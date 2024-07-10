22245fd50ac5efcf64e42952c38509dbc2719d11
Tim Carry <tim@pixelastic.com>
2024-07-10 17:30:21 +0200

    Incorporate jpg into img-min

--- config/zsh/aliases/img.zsh
+++ config/zsh/aliases/img.zsh
@@ -3,7 +3,12 @@ alias eog='img-open'
 alias pinta='gui pinta'
 alias resize="img-resize"
 
-alias ijc="img-jpg-create"
-alias igc="img-gif-create"
+# PNG
 alias ipc="img-png-create"
+alias pngmin="img-min"
+# JPG
+alias ijc="img-jpg-create"
+alias jpgmin="img-min"
+
+alias igc="img-gif-create"
 alias isc="img-svg-create"
