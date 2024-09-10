978e3824f1fefc23f9f3cce559f28219a90b3031
Tim Carry <tim@pixelastic.com>
2024-09-10 20:48:38 +0200

    Move notion to tmp folder

--- config/zsh/env.zsh
+++ config/zsh/env.zsh
@@ -5,6 +5,9 @@ export LANG=en_US.UTF-8               # Default language
 export LC_COLLATE=C                   # Default sort order (in ls, sort, etc)
 export OROSHI_ZSH_PID=$$              # Reference to this zsh pid
 
+# Folder to store files needed for oroshi to run
+export OROSHI_TMP_FOLDER=~/local/tmp/oroshi
+
 # Use a different TERM if used locally on the laptop, or remotely on a server
 if [[ $KITTY_WINDOW_ID != "" ]]; then
 	export TERM="xterm-kitty"
