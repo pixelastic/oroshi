d4ce238ad4b89600c0cade7b57a351a9e803ed79
Tim Carry <tim@pixelastic.com>
2024-09-19 23:43:43 +0200

    Fix some completion

--- config/zsh/prompt/yarn.zsh
+++ config/zsh/prompt/yarn.zsh
@@ -2,34 +2,33 @@
 # Add icons for each known yarn linked project
 function oroshi-prompt-populate:yarn_link() {
   OROSHI_PROMPT_PARTS[yarn_link]=""
-  return
   [[ $GIT_DIRECTORY_IS_REPOSITORY == 0 ]] && return
 
-  local linkedModules="$(yarn-link-list-raw --include-workspace-root)"
-  local totalModuleCount=0
-  local displayedModuleCount=0
+  local linkListRaw="$(yarn-link-list-raw --include-workspace-root)"
+  local totalLinkCount=0
+  local displayedLinkCount=0
   local displayedString=''
 
-  for module in ${(f)linkedModules}; do
-    local split=(${(s/▮/)module})
-    local moduleName=$split[1]
-    local moduleLinkType=$split[2]
+  for rawLine in ${(f)linkListRaw}; do
+    local split=(${(s/▮/)rawLine})
+    local linkName=$split[1]
+    local linkType=$split[2]
 
-    # Keep only the global ones
-    [[ $moduleLinkType = 'local' ]] && continue
-    totalModuleCount=$(($totalModuleCount + 1));
+    # Ignore workspace links
+    [[ $linkType == 'workspace' ]] && continue
+    totalLinkCount=$(($totalLinkCount + 1));
 
     # Add the module icon to the string
-    local projectKey=${moduleName:u}
+    local projectKey=${linkName:u}
     local projectIcon=${(P)${:-PROJECT_${projectKey}_ICON}}
     if [[ $projectIcon != '' ]]; then
       displayedString+="${projectIcon}"
-      displayedModuleCount=$(($displayedModuleCount + 1));
+      displayedLinkCount=$(($displayedLinkCount + 1));
     fi
   done
 
   # If some linked modules don't have an icon, we add the default chain ico
-  if [[ $displayedModuleCount != "$totalModuleCount" ]]; then
+  if [[ $displayedLinkCount != "$totalLinkCount" ]]; then
     displayedString+=" "
   fi
 
