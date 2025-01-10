9d68ee0bdaca4fdda4ca3168270a5eef70a06678
Tim Carry <tim@pixelastic.com>
2025-01-10 12:12:04 +0100

    Change default browser to FF

--- config/zsh/env.zsh
+++ config/zsh/env.zsh
@@ -1,9 +1,9 @@
 # Environment variables {{{
-export EDITOR=nvim                    # Default text editor
-export BROWSER=/usr/bin/google-chrome # Default browser
-export LANG=en_US.UTF-8               # Default language
-export LC_COLLATE=C                   # Default sort order (in ls, sort, etc)
-export OROSHI_ZSH_PID=$$              # Reference to this zsh pid
+export EDITOR=nvim              # Default text editor
+export BROWSER=/usr/bin/firefox # Default browser
+export LANG=en_US.UTF-8         # Default language
+export LC_COLLATE=C             # Default sort order (in ls, sort, etc)
+export OROSHI_ZSH_PID=$$        # Reference to this zsh pid
 
 # Folder to store files needed for oroshi to run
 export OROSHI_TMP_FOLDER=~/local/tmp/oroshi
