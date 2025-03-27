f51ead5e62de80b763b3170da4bec4b025a2bb9d
Tim Carry <tim@pixelastic.com>
2025-03-27 17:24:24 +0100

    Re-order deploy scripts to mirror install scripts

--- config/zsh/aliases/tmux.zsh
+++
@@ -1,7 +0,0 @@
-# tmux
-alias tmsRa='tmux kill-session -a'
-alias tmsR='tmux kill-session -t'
-alias tmsaF='tmux a -dt'
-alias tmsa='tmux attach -t'
-alias tmsc='tmux new -s'
-alias tmsl='tmux list-sessions'
--- scripts/bin/tmux/tmux-refresh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-tmux refresh-client -S
