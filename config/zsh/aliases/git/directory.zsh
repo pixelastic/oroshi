eaf43496c2ca2ed72a443ee40203ccad3f32a2fa
Tim Carry <tim@pixelastic.com>
2025-03-27 11:08:59 +0100

    Add vds: git-directory-sync, to clone or pull a repo

--- config/zsh/aliases/git/directory.zsh
+++ config/zsh/aliases/git/directory.zsh
@@ -1,12 +1,13 @@
 # working [d]irectory
+alias vdca='git-directory-create-all'
 alias vdcl='git tabula-rasa'
 alias vdc='git-directory-create'
-alias vdca='git-directory-create-all'
 alias vde='nvim $(git-directory-root)/.git/config'
 alias vdl='echo "Use vfl instead" && git-file-list' # My muscle memory is here
-alias vdo='git-github-open'
 alias vdos='git-github-open-settings'
+alias vdo='git-github-open'
 alias vdrd='vdr && cd ./docs'
 alias vdrr='vd? && cd "$(git-directory-root -f)"'
 alias vdr='vd? && cd "$(git-directory-root)"'
+alias vds='git-directory-sync'
 alias vd?='git-directory-is-repository'
