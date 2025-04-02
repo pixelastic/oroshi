c78ce969e8a6917f16c3affa482f6fbb7f1b9735
Tim Carry <tim@pixelastic.com>
2025-04-02 12:44:22 +0200

    Fix installation of new git dir

---
+++ config/term/zsh/tools/rg.zsh
@@ -0,0 +1,6 @@
+# rg (ripgrep)
+# rg accepts a config file, but doesn't specifically look in a pre-defined
+# place, like ~/.rgrc, so we manually tell it where to look
+#
+# https://github.com/BurntSushi/ripgrep/blob/master/GUIDE.md#configuration-file
+export RIPGREP_CONFIG_PATH=~/.oroshi/config/cli/rg/dist/rgrc.conf
