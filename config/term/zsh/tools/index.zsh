5de0bbed03d02f84113b89376a9d13f0eb9fea7d
Tim Carry <tim@pixelastic.com>
2025-04-15 15:42:58 +0200

    Update ruby install script

--- config/term/zsh/tools/index.zsh
+++ config/term/zsh/tools/index.zsh
@@ -1,4 +1,4 @@
-# Custom shell tools, like nvm, rvm, etc
+# Custom shell tools, like nvm, rbenv, etc
 for item in $ZSH_CONFIG_PATH/tools/*.zsh; do
 	[[ ${item:t} == "index.zsh" ]] && continue
 	source ${item}
