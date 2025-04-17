f53733a774765580cf987acab581492514a7ddcc
Tim Carry <tim@pixelastic.com>
2025-04-17 15:39:18 +0200

    Add default defsrc

--- config/term/zsh/local/vorugal.zsh
+++ config/term/zsh/local/vorugal.zsh
@@ -1,5 +1,5 @@
 # Reload keybindings
 # Vorugal is the X1 Carbon 12th generation
 function ok() {
-  kmonad ~/.oroshi/config/keybindings/kmonad/kmonad.kbd
+  ~/.oroshi/scripts/deploy/keybindings/kmonad
 }
