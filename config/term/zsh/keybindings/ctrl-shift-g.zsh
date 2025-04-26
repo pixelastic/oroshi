5bcd30e9a5b2f37a3a8ba34c83f9dac34c1f9414
Tim Carry <tim@pixelastic.com>
2025-04-26 16:34:13 +0200

    Fix opening of more than 10 files in vim

--- config/term/zsh/keybindings/ctrl-shift-g.zsh
+++ config/term/zsh/keybindings/ctrl-shift-g.zsh
@@ -16,20 +16,21 @@ oroshi-fzf-regexp-subdir-widget() {
 		return 1
 	fi
 
-	# nvim has no way to open multiple files, each at a different line. The hack
-	# is to open vim, then chain vim commands to open the files files at their
-	# respective lines
-	local nvimArguments=()
+	# nvim has no way to open multiple files, each at a different line.
+	# The trick is to write in a temporary script all the files we want to open,
+	# and then run that script on startup.
+	local tmpScriptPath=$OROSHI_TMP_FOLDER/fzf/nvim-startup-script.tmp
+	rm $tmpScriptPath
+	
 	for item in ${=selection}; do
     local split=(${(@s/:/)item})
 		local filepath=$split[1]
 		local lineNumber=$split[2]
-		nvimArguments+=(-c "tabedit $filepath | $lineNumber")
+		echo "tabedit ${filepath} | ${lineNumber}" >> $tmpScriptPath
 	done
-	# Close the first tab (empty file)
-	nvimArguments+=(-c 'tabfirst | quit!')
+	echo "tabfirst | quit!" >> $tmpScriptPath
 
-	nvim $nvimArguments
+	nvim -S $tmpScriptPath
 
 	return 0
 }
