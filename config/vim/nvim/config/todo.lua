7fc90c3c140c0818bf4259139551dbf0c407e84c
Tim Carry <tim@pixelastic.com>
2025-08-07 00:51:39 +0200

    update todo

--- config/vim/nvim/config/todo.lua
+++ config/vim/nvim/config/todo.lua
@@ -1,30 +1,21 @@
 -- MUST HAVE
 -- TODO: Folding that folds on expr and markers
 -- TODO: Ensure I can complete filepaths
+-- TODO: Add luacheck for linting lua
 -- TODO: Find what works and what does not in a macro
 -- TODO: Completion in commandline, to see fuzzy find of commands
 -- TODO: Keybinding to rename the current file, in a popup
-
--- AI:
 -- TODO: ChatGPT to explain code, quick chat, write tests, write documentation, etc
--- TODO: Test codeium as completion
+-- TODO: Ctrl-! to open the file system should open to where the current file is
+-- TODO: Double check I can go to bottom of script and still have it centered
 
 -- NICE TO HAVE
--- TODO: Ctrl-! to open the file system should open to where the current file is
 -- TODO: Display parent function name in statusbar
 -- TODO: Git capabilities, like vfd in vim?
 -- TODO: Might need to update nvim-treesitter to main branch, instead of master
 -- TODO: Dim the lines when far from the cursor, to help focus? https://github.com/folke/twilight.nvim
--- TODO: Double check I can go to bottom of script and still have it centered
--- TODO: Current line in diff mode has an underline. cf. https://github.com/neovim/neovim/issues/9800
 -- TODO: Add a spinner when thinking in the statusline: https://github.com/folke/noice.nvim/issues/537
 -- TODO: vim.cmd([[cab cc CodeCompanion]]) to remap cc to CodeCompanion in commandline
 -- TODO: Check https://github.com/folke/tokyonight.nvim for methods to lighten/darken colors?
 -- TODO: https://github.com/folke/trouble.nvim ? To see diagnostics
 -- TODO: https://github.com/jpmens/jo create JSON from the CLI
-
--- BIG TOOLS
--- TODO: Add luacheck for linting lua
--- TODO: Add stylua for formatting lua
-
--- LEARNING
