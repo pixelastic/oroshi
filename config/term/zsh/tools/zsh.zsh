64f87d5aaac2de4e3205fc98d5c437b5f852f9a4
Tim Carry <tim@pixelastic.com>
2025-08-06 21:35:10 +0200

    Update color of $()  in term

--- config/term/zsh/tools/zsh.zsh
+++ config/term/zsh/tools/zsh.zsh
@@ -6,87 +6,89 @@ source $ZSH_CONFIG_PATH/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(main)
 
 typeset -A ZSH_HIGHLIGHT_STYLES=(
-	# Fallback color
-	'default' "fg=$COLOR_ALIAS_TEXT"
+  # Fallback color
+  'default' "fg=$COLOR_ALIAS_TEXT"
 
-	# Methods
-	'alias' "fg=$COLOR_ALIAS_FUNCTION"
-	'global-alias' "fg=$COLOR_ALIAS_FUNCTION"
-	'builtin' "fg=$COLOR_ALIAS_FUNCTION"
-	'command' "fg=$COLOR_ALIAS_FUNCTION"
-	'function' "fg=$COLOR_ALIAS_FUNCTION"
-	'reserved-word' "fg=$COLOR_ALIAS_FUNCTION"
+  # Methods
+  'alias' "fg=$COLOR_ALIAS_FUNCTION"
+  'global-alias' "fg=$COLOR_ALIAS_FUNCTION"
+  'builtin' "fg=$COLOR_ALIAS_FUNCTION"
+  'command' "fg=$COLOR_ALIAS_FUNCTION"
+  'function' "fg=$COLOR_ALIAS_FUNCTION"
+  'reserved-word' "fg=$COLOR_ALIAS_FUNCTION"
 
-	# Arguments
-	'double-hyphen-option' "fg=$COLOR_ALIAS_FLAG"
-	'single-hyphen-option' "fg=$COLOR_ALIAS_FLAG"
+  # Arguments
+  'double-hyphen-option' "fg=$COLOR_ALIAS_FLAG"
+  'single-hyphen-option' "fg=$COLOR_ALIAS_FLAG"
 
-	# Variables
-	'dollar-double-quoted-argument' "fg=$COLOR_ALIAS_INTERPOLATION_VARIABLE"
-	'assign' "fg=$COLOR_ALIAS_INTERPOLATION_VARIABLE"
+  # Variables
+  'dollar-double-quoted-argument' "fg=$COLOR_ALIAS_INTERPOLATION_VARIABLE"
+  'assign' "fg=$COLOR_ALIAS_INTERPOLATION_VARIABLE"
 
-	# Punctuation
-	'back-double-quoted-argument' "fg=$COLOR_ALIAS_PUNCTUATION"
-	'commandseparator' "fg=$COLOR_ALIAS_PUNCTUATION"
+  # $() interpolation
+  'command-substitution' "fg=$COLOR_ALIAS_INTERPOLATION_WRAPPER"
+  'command-substitution-delimiter' "fg=$COLOR_ALIAS_INTERPOLATION_WRAPPER"
 
-	# Path
-	'path' "fg=$COLOR_ALIAS_DIRECTORY"                      # Existing path
-	'path_pathseparator' "fg=$COLOR_ALIAS_DIRECTORY"        # / in existing path
-	'path_prefix' "fg=$COLOR_ALIAS_DIRECTORY"               # Incomplete path
-	'path_prefix_pathseparator' "fg=$COLOR_ALIAS_DIRECTORY" # / in incomplete path
-	'autodirectory' "fg=$COLOR_ALIAS_DIRECTORY"
+  # Punctuation
+  'back-double-quoted-argument' "fg=$COLOR_ALIAS_PUNCTUATION"
+  'commandseparator' "fg=$COLOR_ALIAS_PUNCTUATION"
 
-	# Glob
-	'globbing' "fg=$COLOR_ALIAS_GLOB"
+  # Path
+  'path' "fg=$COLOR_ALIAS_DIRECTORY"                      # Existing path
+  'path_pathseparator' "fg=$COLOR_ALIAS_DIRECTORY"        # / in existing path
+  'path_prefix' "fg=$COLOR_ALIAS_DIRECTORY"               # Incomplete path
+  'path_prefix_pathseparator' "fg=$COLOR_ALIAS_DIRECTORY" # / in incomplete path
+  'autodirectory' "fg=$COLOR_ALIAS_DIRECTORY"
 
-	# Strings (blue)
-	'back-quoted-argument' "fg=$COLOR_ALIAS_STRING"
-	'back-quoted-argument-unclosed' "bg=$COLOR_ALIAS_STRING,fg=$COLOR_BLACK"
-	'back-quoted-argument-delimiter' "fg=$COLOR_ALIAS_STRING"
-	'double-quoted-argument' "fg=$COLOR_ALIAS_STRING"
-	'double-quoted-argument-unclosed' "bg=$COLOR_ALIAS_STRING,fg=$COLOR_BLACK"
-	'single-quoted-argument' "fg=$COLOR_ALIAS_STRING"
-	'single-quoted-argument-unclosed' "bg=$COLOR_ALIAS_STRING,fg=$COLOR_BLACK"
+  # Glob
+  'globbing' "fg=$COLOR_ALIAS_GLOB"
 
-	# Numbers (bold blue)
-	'arithmetic-expansion' "fg=$COLOR_ALIAS_NUMBER"
+  # Strings (blue)
+  'back-quoted-argument' "fg=$COLOR_ALIAS_STRING"
+  'back-quoted-argument-unclosed' "bg=$COLOR_ALIAS_STRING,fg=$COLOR_BLACK"
+  'back-quoted-argument-delimiter' "fg=$COLOR_ALIAS_STRING"
+  'double-quoted-argument' "fg=$COLOR_ALIAS_STRING"
+  'double-quoted-argument-unclosed' "bg=$COLOR_ALIAS_STRING,fg=$COLOR_BLACK"
+  'single-quoted-argument' "fg=$COLOR_ALIAS_STRING"
+  'single-quoted-argument-unclosed' "bg=$COLOR_ALIAS_STRING,fg=$COLOR_BLACK"
 
-	# Repetition of last command using !
-	'history-expansion' "fg=$COLOR_NEUTRAL"
+  # Numbers (bold blue)
+  'arithmetic-expansion' "fg=$COLOR_ALIAS_NUMBER"
 
-	# Errors
-	'unknown-token' "fg=$COLOR_ALIAS_ERROR"
+  # Repetition of last command using !
+  'history-expansion' "fg=$COLOR_NEUTRAL"
 
-	# sudo
-	'precommand' "fg=$COLOR_ALIAS_WARNING,bold"
+  # Errors
+  'unknown-token' "fg=$COLOR_ALIAS_ERROR"
 
-	# &>, 1>, 2> redirection
-	'redirection' "fg=$COLOR_ALIAS_SYMBOL"
+  # sudo
+  'precommand' "fg=$COLOR_ALIAS_WARNING,bold"
 
-	# ???
-	'arg0' "fg=$COLOR_ALIAS_UNKNOWN"
-	'arithmetic-expansion' "fg=$COLOR_ALIAS_UNKNOWN"
-	'back-dollar-quoted-argument' "fg=$COLOR_ALIAS_UNKNOWN"
-	'bracket-error' "fg=$COLOR_ALIAS_UNKNOWN"
-	'bracket-level-1' "fg=$COLOR_ALIAS_UNKNOWN"
-	'bracket-level-2' "fg=$COLOR_ALIAS_UNKNOWN"
-	'bracket-level-3' "fg=$COLOR_ALIAS_UNKNOWN"
-	'bracket-level-4' "fg=$COLOR_ALIAS_UNKNOWN"
-	'bracket-level-5' "fg=$COLOR_ALIAS_UNKNOWN"
-	'command-substitution' "fg=$COLOR_ALIAS_UNKNOWN"
-	'command-substitution-delimiter' "fg=$COLOR_ALIAS_UNKNOWN"
-	'comment' "fg=$COLOR_ALIAS_UNKNOWN"
-	'cursor' "fg=$COLOR_ALIAS_UNKNOWN"
-	'cursor-matchingbracket' "fg=$COLOR_ALIAS_UNKNOWN"
-	'dollar-quoted-argument' "fg=$COLOR_ALIAS_UNKNOWN"
-	'hashed-command' "fg=$COLOR_ALIAS_UNKNOWN"
-	'line' "fg=$COLOR_ALIAS_UNKNOWN"
-	'named-fd' "fg=$COLOR_ALIAS_UNKNOWN"
-	'numeric-fd' "fg=$COLOR_ALIAS_UNKNOWN"
-	'path_approx' "fg=$COLOR_ALIAS_UNKNOWN"
-	'process-substitution' "fg=$COLOR_ALIAS_UNKNOWN"
-	'process-substitution-delimiter' "fg=$COLOR_ALIAS_UNKNOWN"
-	'rc-quote' "fg=$COLOR_ALIAS_UNKNOWN"
-	'root' "fg=$COLOR_ALIAS_UNKNOWN"
-	'suffix-alias' "fg=$COLOR_ALIAS_UNKNOWN"
+  # &>, 1>, 2> redirection
+  'redirection' "fg=$COLOR_ALIAS_SYMBOL"
+
+  # ???
+  'arg0' "fg=$COLOR_ALIAS_UNKNOWN"
+  'arithmetic-expansion' "fg=$COLOR_ALIAS_UNKNOWN"
+  'back-dollar-quoted-argument' "fg=$COLOR_ALIAS_UNKNOWN"
+  'bracket-error' "fg=$COLOR_ALIAS_UNKNOWN"
+  'bracket-level-1' "fg=$COLOR_ALIAS_UNKNOWN"
+  'bracket-level-2' "fg=$COLOR_ALIAS_UNKNOWN"
+  'bracket-level-3' "fg=$COLOR_ALIAS_UNKNOWN"
+  'bracket-level-4' "fg=$COLOR_ALIAS_UNKNOWN"
+  'bracket-level-5' "fg=$COLOR_ALIAS_UNKNOWN"
+  'comment' "fg=$COLOR_ALIAS_UNKNOWN"
+  'cursor' "fg=$COLOR_ALIAS_UNKNOWN"
+  'cursor-matchingbracket' "fg=$COLOR_ALIAS_UNKNOWN"
+  'dollar-quoted-argument' "fg=$COLOR_ALIAS_UNKNOWN"
+  'hashed-command' "fg=$COLOR_ALIAS_UNKNOWN"
+  'line' "fg=$COLOR_ALIAS_UNKNOWN"
+  'named-fd' "fg=$COLOR_ALIAS_UNKNOWN"
+  'numeric-fd' "fg=$COLOR_ALIAS_UNKNOWN"
+  'path_approx' "fg=$COLOR_ALIAS_UNKNOWN"
+  'process-substitution' "fg=$COLOR_ALIAS_UNKNOWN"
+  'process-substitution-delimiter' "fg=$COLOR_ALIAS_UNKNOWN"
+  'rc-quote' "fg=$COLOR_ALIAS_UNKNOWN"
+  'root' "fg=$COLOR_ALIAS_UNKNOWN"
+  'suffix-alias' "fg=$COLOR_ALIAS_UNKNOWN"
 )
