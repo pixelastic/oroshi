08ed193b03796ae9fb67cfc144abe0d50245ea4d
Tim Carry <tim@pixelastic.com>
2025-08-06 23:55:53 +0200

    update TODO

--- config/vim/nvim/config/todo.lua
+++ config/vim/nvim/config/todo.lua
@@ -1,33 +1,30 @@
 -- MUST HAVE
--- TODO: Auto-formatter on save
 -- TODO: Folding that folds on expr and markers
--- TODO: https://github.com/folke/trouble.nvim ? To see diagnostics
--- TODO: Check https://github.com/folke/tokyonight.nvim for methods to lighten/darken colors?
+-- TODO: Ensure I can complete filepaths
+-- TODO: Find what works and what does not in a macro
 -- TODO: Completion in commandline, to see fuzzy find of commands
--- TODO: https://github.com/jpmens/jo create JSON from the CLI
 -- TODO: Keybinding to rename the current file, in a popup
 
 -- AI:
 -- TODO: ChatGPT to explain code, quick chat, write tests, write documentation, etc
 -- TODO: Test codeium as completion
--- TODO: Use mini.diff to have inplace diff with CodeCompanion inline
 
 -- NICE TO HAVE
--- TODO: saving a JSON file should remove trailing commas
 -- TODO: Ctrl-! to open the file system should open to where the current file is
 -- TODO: Display parent function name in statusbar
 -- TODO: Git capabilities, like vfd in vim?
--- TODO: Ensure I can complete filepaths
 -- TODO: Might need to update nvim-treesitter to main branch, instead of master
 -- TODO: Dim the lines when far from the cursor, to help focus? https://github.com/folke/twilight.nvim
 -- TODO: Double check I can go to bottom of script and still have it centered
 -- TODO: Current line in diff mode has an underline. cf. https://github.com/neovim/neovim/issues/9800
 -- TODO: Add a spinner when thinking in the statusline: https://github.com/folke/noice.nvim/issues/537
 -- TODO: vim.cmd([[cab cc CodeCompanion]]) to remap cc to CodeCompanion in commandline
+-- TODO: Check https://github.com/folke/tokyonight.nvim for methods to lighten/darken colors?
+-- TODO: https://github.com/folke/trouble.nvim ? To see diagnostics
+-- TODO: https://github.com/jpmens/jo create JSON from the CLI
 
 -- BIG TOOLS
 -- TODO: Add luacheck for linting lua
 -- TODO: Add stylua for formatting lua
 
 -- LEARNING
--- TODO: Find what works and what does not in a macro
