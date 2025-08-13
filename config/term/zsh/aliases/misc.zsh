f46cebe0b5b8465673c8280a31bd5a665e937be1
Tim Carry <tim@pixelastic.com>
2025-08-13 09:56:41 +0200

    Add uuid alias

--- config/term/zsh/aliases/misc.zsh
+++ config/term/zsh/aliases/misc.zsh
@@ -1,23 +1,23 @@
 # misc
 
 alias diff='git diff --no-index --word-diff=color --word-diff-regex=.'
-alias ping="prettyping --nolegend"
-alias serve='live-server'
-alias sr='search-and-replace'
 alias dim="dimensions"
-disable -r time # ZSH overrides the time method
-alias time='time -p '
-alias w='which'
 alias disk-space='duf'
-alias mkdir="mkdir -p"
-alias p8='ping 8.8.8.8'
-alias n="nnn"
-alias virtualbox='gui virtualbox'
-alias vlc='better-vlc'
-alias ydotool='better-ydotool'
+alias emote='flatpak run com.tomjwatson.Emote'
 alias flushdns="/etc/init.d/dns-clean start"
 alias htmlmin="html-minifier --remove-comments --collapse-whitespace --remove-attribute-quotes --remove-redundant-attributes --use-short-doctype"
-alias speedtest='wget -O /dev/null http://speedtest.wdc01.softlayer.com/downloads/test500.zip'
-# alias htop="htop --delay=10000"
-alias emote='flatpak run com.tomjwatson.Emote'
 alias lunii='studio'
+alias mkdir="mkdir -p"
+alias n="nnn"
+alias p8='ping 8.8.8.8'
+alias ping="prettyping --nolegend"
+alias serve='live-server'
+alias speedtest='wget -O /dev/null http://speedtest.wdc01.softlayer.com/downloads/test500.zip'
+alias sr='search-and-replace'
+alias time='time -p '
+alias virtualbox='gui virtualbox'
+alias vlc='better-vlc'
+alias w='which'
+alias ydotool='better-ydotool'
+alias uuid="uuidgen"
+disable -r time # ZSH overrides the time method
