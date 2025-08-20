d669e8b01f1cd97bc2dcfed09130262e98d970f8
Tim Carry <tim@pixelastic.com>
2025-08-20 13:40:09 +0200

    Adding a flylint

--- cheats/zsh/trim.zsh
+++ cheats/zsh/trim.zsh
@@ -1,6 +1,5 @@
 # Trim a string
 local value="   Hello World     "
 
-value="${value#"${value%%[![:space:]]*}"}"  # Trim spaces at beginning
-value="${value%"${value##*[![:space:]]}"}"  # Trip spaces at end
-echo $value
+value="${value#"${value%%[![:space:]]*}"}" # Trim spaces at beginning
+value="${value%"${value##*[![:space:]]}"}" # Trip spaces at end
