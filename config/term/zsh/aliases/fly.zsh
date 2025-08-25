b27e9f46c93f9372abb7d798a54f70f6d8e71261
Tim Carry <tim@pixelastic.com>
2025-08-25 13:21:39 +0200

    Add fly helper methods

---
+++ config/term/zsh/aliases/fly.zsh
@@ -0,0 +1,21 @@
+# Fly.io aliases
+
+# App
+alias fal='fly-app-list'
+alias fad='fly-app-deploy'
+alias fal='fly-app-logs'
+alias fan='fly-app-name'
+alias fao='fly-app-open'
+alias fas='fly-app-status'
+
+# Config
+alias fct='fly-config-test'
+alias fcp='fly-config-path'
+
+# Directory
+alias fdr='fly-directory-root'
+
+# Secrets
+alias fsc='fly-secret-create'
+alias fsl='fly-secret-list'
+alias fsR='fly-secret-remove'
