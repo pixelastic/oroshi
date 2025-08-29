578fa6cc17f6a0220e4d0b4a0b0f14a5d2decd18
Tim Carry <tim@pixelastic.com>
2025-08-29 21:51:17 +0200

    Add ## for console.log in js

--- config/vim/nvim/config/filetypes/javascript.lua
+++ config/vim/nvim/config/filetypes/javascript.lua
@@ -1,4 +1,5 @@
 -- zsh
 F.ftplugin("javascript", function()
   F.imap("$ù", "console.log(", "Console log", { buffer = F.bufferId() })
+  F.imap("##", "${}<Left>", "Create interpolated variable", { buffer = F.bufferId() })
 end)
