cac49efcc54800117b44543a81f523d82a724f62
Tim Carry <tim@pixelastic.com>
2025-09-05 10:18:34 +0200

    Make sure tree keeps colors with L

--- config/term/zsh/aliases/tree.zsh
+++ config/term/zsh/aliases/tree.zsh
@@ -1,5 +1,5 @@
 # Tree
-alias tree='better-ls --tree'
-alias treed='better-ls --tree --only-dirs'
+alias tree='better-ls --tree --color=always'
+alias treed='better-ls --tree --only-dirs --color=always'
 alias watch='watch -c '
 alias wr='watch-and-reload'
