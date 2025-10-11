47cf4fdf1b2e4ef73742e9df8f0c3509f72680a7
Tim Carry <tim@pixelastic.com>
2025-10-11 19:23:02 +0200

    Allow toggling model used by mic2txt

--- config/term/kitty/tab_bar.py
+++ config/term/kitty/tab_bar.py
@@ -25,6 +25,7 @@ KITTY_OPTIONS = get_options()
 # Note: use kitty-refresh script to force-refresh the display for debugging
 STATUSBAR_DEFINITION = [
     # "spotify:5",
+    "mic2txt-model:60",
     "sound-mode:60",
     "battery:60",
     "cpu:30",
