066bd474e522aa048174d18f0b961ea5062e894d
Tim Carry <tim@pixelastic.com>
2025-10-19 00:06:40 +0200

    More robust global node binaries detection

--- config/term/zsh/path.zsh
+++ config/term/zsh/path.zsh
@@ -1,73 +1,69 @@
 # Define a custom $PATH variable that look for all our binaries
 function oroshi_path() {
-	local hostname="$(hostname)"
+  local hostname="$(hostname)"
 
-	# Build the list of subdirs in ./scripts/bin
-	local localBinariesPath=()
-	for directory in ~/.oroshi/scripts/bin/**/; do
-		# Skip all directories starting with __
-		[[ $directory == */__* ]] && continue
-		# As well as node_modules added by zx
-		[[ $directory == */node_modules* ]] && continue
+  # Build the list of subdirs in ./scripts/bin
+  local localBinariesPath=()
+  for directory in ~/.oroshi/scripts/bin/**/; do
+    # Skip all directories starting with __
+    [[ $directory == */__* ]] && continue
+    # As well as node_modules added by zx
+    [[ $directory == */node_modules* ]] && continue
 
-		localBinariesPath+=(${directory:0:-1})
-	done
+    localBinariesPath+=(${directory:0:-1})
+  done
 
-	# Node
-	if [[ -f ~/.nvm/alias/default ]]; then
-		# The default node version to use is the one marked as "default" in nvm
-		# Note that this file is only created when a node version is being set as
-		# default
-		local defaultNodeVersion="$(<~/.nvm/alias/default)"
-		local nodeBinariesPath=$HOME/.nvm/versions/node/v${defaultNodeVersion}/bin
-	fi
+  # Node
+  if [[ -f ~/.nvm/alias/default ]]; then
+    local nodeBinariesPath=$OROSHI_TMP_FOLDER/node/bin
+  fi
 
-	# Python
-	if [[ -d ~/.pyenv/ ]]; then
-		# The default python version to use is the one saved in ~/.pyenv/version Note:
-		# The default method of using pyenv is relying on its shims, but they spawn so
-		# many wrapped bash shell scripts that using them adds 1-2 seconds delay to
-		# each python command. Instead, we specifically add the binaries from the
-		# current version to the PATH
-		local defaultPythonVersion="$(<~/.pyenv/version)"
-		local pythonBinariesPath=$HOME/.pyenv/versions/${defaultPythonVersion}/bin
-	fi
+  # Python
+  if [[ -d ~/.pyenv/ ]]; then
+    # The default python version to use is the one saved in ~/.pyenv/version Note:
+    # The default method of using pyenv is relying on its shims, but they spawn so
+    # many wrapped bash shell scripts that using them adds 1-2 seconds delay to
+    # each python command. Instead, we specifically add the binaries from the
+    # current version to the PATH
+    local defaultPythonVersion="$(<~/.pyenv/version)"
+    local pythonBinariesPath=$HOME/.pyenv/versions/${defaultPythonVersion}/bin
+  fi
 
-	path=(
-		# Local binaries
-		~/.oroshi/private/scripts/bin/local/$hostname
+  path=(
+    # Local binaries
+    ~/.oroshi/private/scripts/bin/local/$hostname
 
-		# Private binaries
-		~/.oroshi/private/scripts/bin
+    # Private binaries
+    ~/.oroshi/private/scripts/bin
 
-		# Custom binaries
-		$localBinariesPath
+    # Custom binaries
+    $localBinariesPath
 
-		# Installed binaries
-		~/local/bin
-		~/.local/bin
-		~/local/src/fzf/bin
+    # Installed binaries
+    ~/local/bin
+    ~/.local/bin
+    ~/local/src/fzf/bin
 
-		# Language binaries
-		$nodeBinariesPath
-		~/.rbenv/bin
-		~/.rbenv/shims
-		~/.pyenv/bin
-		$pythonBinariesPath
-		~/.cargo/bin
+    # Language binaries
+    $nodeBinariesPath
+    ~/.rbenv/bin
+    ~/.rbenv/shims
+    ~/.pyenv/bin
+    $pythonBinariesPath
+    ~/.cargo/bin
 
-		# System paths
-		# (Check /etc/environment for the exact list)
-		/usr/local/sbin
-		/usr/local/bin
-		/usr/sbin
-		/usr/bin
-		/sbin
-		/bin
-		/usr/games
-		/usr/local/games
-		/snap/bin
-	)
+    # System paths
+    # (Check /etc/environment for the exact list)
+    /usr/local/sbin
+    /usr/local/bin
+    /usr/sbin
+    /usr/bin
+    /sbin
+    /bin
+    /usr/games
+    /usr/local/games
+    /snap/bin
+  )
 }
 oroshi_path
 unfunction oroshi_path
