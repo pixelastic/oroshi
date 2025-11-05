f686c52e7f50246c98f6790447fa2ffdd7916b8c
Tim Carry <tim@pixelastic.com>
2025-11-05 18:02:28 +0100

    Fix lua linting

--- config/vim/nvim/config/plugins/helpers/filetypes/lua.lua
+++ config/vim/nvim/config/plugins/helpers/filetypes/lua.lua
@@ -3,15 +3,9 @@ local M = {}
 local helper = O_require("oroshi/plugins/helpers/code-quality")
 
 M.configureLsp = function()
-  helper.configureLspServer("lua_ls", {
-    on_init = function(client)
-      local config = {
-        runtime = { version = "LuaJIT" },
-        workspace = { checkThirdParty = false, library = { vim.env.VIMRUNTIME } },
-      }
-      client.config.settings.Lua = F.merge(client.config.settings.Lua or {}, config)
-    end,
-  })
+  -- Check .luarc.json for details of the exact config. This applies only to
+  -- .lua files used to configure Neovim.
+  helper.configureLspServer("lua_ls", {})
 end
 
 M.configureFormatter = function(conform)
