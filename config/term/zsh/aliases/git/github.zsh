fcb3d1002e61647eb245222c19a620091171e089
Tim Carry <tim@pixelastic.com>
2026-02-27 18:04:52 +0100

    fix(zsh): rename git-github-ratelimit to git-github-rate-limit
    Improves consistency with other command naming patterns using hyphens
    instead of compound words. Updates alias and function name accordingly.

--- config/term/zsh/aliases/git/github.zsh
+++ config/term/zsh/aliases/git/github.zsh
@@ -2,7 +2,7 @@
 alias vghapi='git-github-api'
 alias vghbd='git-github-branch-default'
 alias vghbdu='git-github-branch-default-update'
-alias vghrl='git-github-ratelimit'
+alias vghrl='git-github-rate-limit'
 alias vgho="git-github-open"
 alias vghos="git-github-open-settings"
 alias vghoi="git-github-open-issues"
