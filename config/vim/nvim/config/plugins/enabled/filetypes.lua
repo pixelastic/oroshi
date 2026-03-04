b57e5ec7ab6081bee026fe783927eee568034bb7
Tim Carry <tim@pixelastic.com>
2026-03-04 13:25:18 +0100

    feat(vim): add context-aware commenting for Vue files
    Enables proper comment handling within Vue single-file components by
    dynamically calculating commentstring based on cursor position.

--- config/vim/nvim/config/plugins/enabled/filetypes.lua
+++ config/vim/nvim/config/plugins/enabled/filetypes.lua
@@ -28,4 +28,29 @@ return {
       })
     end,
   },
+
+  -- Vue
+  -- https://github.com/JoosepAlviste/nvim-ts-context-commentstring
+  -- Allow changing commentstring based on position in file
+  {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    config = function()
+      local nvimTsContextCommentstring = require("ts_context_commentstring")
+      -- Disable CursoHold autocommand of the plugin
+      nvimTsContextCommentstring.setup({
+        enable_autocmd = false,
+      })
+
+      -- Overwrite get_option to recalculate commentstring based on position in file
+      local originalGetOption = vim.filetype.get_option
+      vim.filetype.get_option = function(filetype, option)
+        if option ~= "commentstring" then
+          return originalGetOption(filetype, option)
+        end
+
+        return nvimTsContextCommentstring.calculate_commentstring()
+      end
+    end,
+  },
 }
