0022af7071d428f4c614a2d9fbfcd6b4eab6e1cc
Tim Carry <tim@pixelastic.com>
2026-04-16 23:34:07 +0200

    perf(zsh): lazy load GVM to improve startup time
    GVM now only loads when go/gvm commands are actually used,
    preventing ~100ms+ startup delay for sessions that don't need Go.

--- config/term/zsh/tools/gvm.zsh
+++ config/term/zsh/tools/gvm.zsh
@@ -1,9 +1,39 @@
-# Gvm
-# Currently disabling this, as I don't much go
-# TODO: Make this smarter, like the lazyloading we use for JavaScript
+# GVM - Lazy Loading
+# Load GVM only when actually needed to improve ZSH startup time
+export OROSHI_GVM_LOADED="0"
 
-# Switch go version
+# Stop if gvm isn't installed
 local gvmPath=~/.gvm/scripts/gvm
 [[ -r $gvmPath ]] || return
 
-source $gvmPath
+# List of commands that should trigger GVM loading
+# Main commands: gvm, go
+export OROSHI_GVM_LAZYLOAD_ALIASES=(
+  gvm
+  go
+)
+
+# Create lazy-loading aliases for all these commands
+for command in $OROSHI_GVM_LAZYLOAD_ALIASES; do
+  alias $command="lazyloadGvm $command"
+done
+
+function lazyloadGvm {
+  # Prevent infinite loop if already loaded
+  if [[ $OROSHI_GVM_LOADED == "1" ]]; then
+    "$@"
+    return
+  fi
+
+  # Unregister all the aliases so commands refer to real binaries now
+  unalias $OROSHI_GVM_LAZYLOAD_ALIASES
+
+  # Source gvm for real
+  source $gvmPath
+
+  # Mark gvm as loaded
+  OROSHI_GVM_LOADED="1"
+
+  # Run the initial command that triggered the load
+  "$@"
+}
