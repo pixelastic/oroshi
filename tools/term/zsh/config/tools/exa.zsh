1dd486515772793bb03f47d0876f5c4f6dfe08ed
Tim Carry <tim@pixelastic.com>
2026-06-09 21:39:49 +0200

    refactor(exa): wrap color configuration in a scoped function
    Color definitions are now set inside a function that calls
    `colors-load-definitions` before building `EXA_COLORS`, ensuring the
    color palette is loaded before use. The function is unset immediately
    after invocation to avoid polluting the shell namespace.

--- tools/term/zsh/config/tools/exa.zsh
+++ tools/term/zsh/config/tools/exa.zsh
@@ -1,47 +1,52 @@
 # Exa
 #
 # See https://the.exa.website/docs/colour-themes
+function exa-configure-colors() {
+  colors-load-definitions
 
-EXA_COLORS=""
-EXA_COLORS="${EXA_COLORS}:xx=38;5;$COLORS[ui]" # Punctuation
+  EXA_COLORS=""
+  EXA_COLORS="${EXA_COLORS}:xx=38;5;$COLORS[ui]" # Punctuation
 
-EXA_COLORS="${EXA_COLORS}:fi=38;5;$COLORS[gray-white]" # Default file color
+  EXA_COLORS="${EXA_COLORS}:fi=38;5;$COLORS[gray-white]" # Default file color
 
-EXA_COLORS="${EXA_COLORS}:ur=38;5;$COLORS[gray-7]"   # User read
-EXA_COLORS="${EXA_COLORS}:uw=38;5;$COLORS[gray-7]"   # User write
-EXA_COLORS="${EXA_COLORS}:ux=4;38;5;$COLORS[purple]" # User execute
+  EXA_COLORS="${EXA_COLORS}:ur=38;5;$COLORS[gray-7]"   # User read
+  EXA_COLORS="${EXA_COLORS}:uw=38;5;$COLORS[gray-7]"   # User write
+  EXA_COLORS="${EXA_COLORS}:ux=4;38;5;$COLORS[purple]" # User execute
 
-EXA_COLORS="${EXA_COLORS}:gr=38;5;$COLORS[gray-7]"   # Group read
-EXA_COLORS="${EXA_COLORS}:gw=38;5;$COLORS[gray-7]"   # Group write
-EXA_COLORS="${EXA_COLORS}:gx=4;38;5;$COLORS[purple]" # Group execute
+  EXA_COLORS="${EXA_COLORS}:gr=38;5;$COLORS[gray-7]"   # Group read
+  EXA_COLORS="${EXA_COLORS}:gw=38;5;$COLORS[gray-7]"   # Group write
+  EXA_COLORS="${EXA_COLORS}:gx=4;38;5;$COLORS[purple]" # Group execute
 
-EXA_COLORS="${EXA_COLORS}:tr=38;5;$COLORS[gray-7]"   # Other read
-EXA_COLORS="${EXA_COLORS}:tw=38;5;$COLORS[gray-7]"   # Other write
-EXA_COLORS="${EXA_COLORS}:tx=4;38;5;$COLORS[purple]" # Other execute
+  EXA_COLORS="${EXA_COLORS}:tr=38;5;$COLORS[gray-7]"   # Other read
+  EXA_COLORS="${EXA_COLORS}:tw=38;5;$COLORS[gray-7]"   # Other write
+  EXA_COLORS="${EXA_COLORS}:tx=4;38;5;$COLORS[purple]" # Other execute
 
-EXA_COLORS="${EXA_COLORS}:sn=38;5;$COLORS[neutral]"       # Size
-EXA_COLORS="${EXA_COLORS}:sb=38;5;$COLORS[neutral-light]" # Size unit
+  EXA_COLORS="${EXA_COLORS}:sn=38;5;$COLORS[neutral]"       # Size
+  EXA_COLORS="${EXA_COLORS}:sb=38;5;$COLORS[neutral-light]" # Size unit
 
-EXA_COLORS="${EXA_COLORS}:uu=38;5;$COLORS[black]" # Me
-EXA_COLORS="${EXA_COLORS}:un=38;5;$COLORS[red-5]" # Not me
+  EXA_COLORS="${EXA_COLORS}:uu=38;5;$COLORS[black]" # Me
+  EXA_COLORS="${EXA_COLORS}:un=38;5;$COLORS[red-5]" # Not me
 
-EXA_COLORS="${EXA_COLORS}:da=38;5;$COLORS[date]" # Date
+  EXA_COLORS="${EXA_COLORS}:da=38;5;$COLORS[date]" # Date
 
-EXA_COLORS="${EXA_COLORS}:ga=38;5;$COLORS[git-added]"     # File added
-EXA_COLORS="${EXA_COLORS}:gm=38;5;$COLORS[git-modified]"  # File modified
-EXA_COLORS="${EXA_COLORS}:gd=38;5;$COLORS[git-removed]"   # File Deleted
-EXA_COLORS="${EXA_COLORS}:gv=38;5;$COLORS[git-modified]"  # File renamed
-EXA_COLORS="${EXA_COLORS}:gt=38;5;$COLORS[variable-type]" # File type changed
+  EXA_COLORS="${EXA_COLORS}:ga=38;5;$COLORS[git-added]"     # File added
+  EXA_COLORS="${EXA_COLORS}:gm=38;5;$COLORS[git-modified]"  # File modified
+  EXA_COLORS="${EXA_COLORS}:gd=38;5;$COLORS[git-removed]"   # File Deleted
+  EXA_COLORS="${EXA_COLORS}:gv=38;5;$COLORS[git-modified]"  # File renamed
+  EXA_COLORS="${EXA_COLORS}:gt=38;5;$COLORS[variable-type]" # File type changed
 
-EXA_COLORS="${EXA_COLORS}:di=38;5;$COLORS[green]"      # Directories
-EXA_COLORS="${EXA_COLORS}:ex=4;38;5;$COLORS[violet-4]" # Executable
+  EXA_COLORS="${EXA_COLORS}:di=38;5;$COLORS[green]"      # Directories
+  EXA_COLORS="${EXA_COLORS}:ex=4;38;5;$COLORS[violet-4]" # Executable
 
-# EXA_COLORS="${EXA_COLORS}:ln=38;5;69"  # Symlink source
-# EXA_COLORS="${EXA_COLORS}:lp=38;5;69"  # Symlink destination
-# EXA_COLORS="${EXA_COLORS}:or=38;5;160" # Broken symlink
-# EXA_COLORS="${EXA_COLORS}:pi=48;5;13"  # Named pipe (unstyled)
-# EXA_COLORS="${EXA_COLORS}:cd=48;5;13"  # Character device (unstyled)
-# EXA_COLORS="${EXA_COLORS}:so=48;5;13"  # Socket (unstyled)
-# EXA_COLORS="${EXA_COLORS}:bd=48;5;13"  # Block device (unstyled)
+  # EXA_COLORS="${EXA_COLORS}:ln=38;5;69"  # Symlink source
+  # EXA_COLORS="${EXA_COLORS}:lp=38;5;69"  # Symlink destination
+  # EXA_COLORS="${EXA_COLORS}:or=38;5;160" # Broken symlink
+  # EXA_COLORS="${EXA_COLORS}:pi=48;5;13"  # Named pipe (unstyled)
+  # EXA_COLORS="${EXA_COLORS}:cd=48;5;13"  # Character device (unstyled)
+  # EXA_COLORS="${EXA_COLORS}:so=48;5;13"  # Socket (unstyled)
+  # EXA_COLORS="${EXA_COLORS}:bd=48;5;13"  # Block device (unstyled)
 
-export EXA_COLORS
+  export EXA_COLORS
+}
+exa-configure-colors
+unfunction exa-configure-colors
