a35c5464f4c006c88b9f00293ba7156d0422b736
Tim Carry <tim@pixelastic.com>
2026-06-12 16:54:09 +0200

    chore(scripts): remove music management scripts and tooling
    All audio conversion utilities, music library management scripts, and
    their supporting engines have been deleted. This removes a significant
    body of unmaintained tooling that is no longer needed.

--- tools/term/zsh/config/aliases/music.zsh
+++ tools/term/zsh/config/aliases/music.zsh
@@ -3,7 +3,3 @@ alias spoify='spotify'
 alias spotufy='spotify'
 alias spotofy='spotify'
 alias spot='spotify'
-# Music library handling
-alias mmu='music-metadata-update'
-alias mktl='generate-tracklist'
-alias mm="music-mark"
