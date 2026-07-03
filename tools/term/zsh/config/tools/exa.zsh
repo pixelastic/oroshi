5f260840e9415139ef7ed356b70d1fac543ddc28
Tim Carry <tim@pixelastic.com>
2026-07-03 23:37:03 +0200

    style(zsh): remap filetype and ls colors to named color tokens
    Config, image, and JS filetypes now reference distinct named palette
    entries instead of shared raw color codes, and all special file types in
    ls/exa use a unified `unknown` token. This eliminates implicit color
    aliasing and makes future palette-wide recoloring possible from a single
    source.

--- tools/term/zsh/config/tools/exa.zsh
+++ tools/term/zsh/config/tools/exa.zsh
@@ -9,17 +9,17 @@ function exa-configure-colors() {
 
   EXA_COLORS="${EXA_COLORS}:fi=38;5;$COLORS[gray-white]" # Default file color
 
-  EXA_COLORS="${EXA_COLORS}:ur=38;5;$COLORS[gray-7]"   # User read
-  EXA_COLORS="${EXA_COLORS}:uw=38;5;$COLORS[gray-7]"   # User write
-  EXA_COLORS="${EXA_COLORS}:ux=4;38;5;$COLORS[purple]" # User execute
+  EXA_COLORS="${EXA_COLORS}:ur=38;5;$COLORS[gray-7]"       # User read
+  EXA_COLORS="${EXA_COLORS}:uw=38;5;$COLORS[gray-7]"       # User write
+  EXA_COLORS="${EXA_COLORS}:ux=4;38;5;$COLORS[executable]" # User execute
 
-  EXA_COLORS="${EXA_COLORS}:gr=38;5;$COLORS[gray-7]"   # Group read
-  EXA_COLORS="${EXA_COLORS}:gw=38;5;$COLORS[gray-7]"   # Group write
-  EXA_COLORS="${EXA_COLORS}:gx=4;38;5;$COLORS[purple]" # Group execute
+  EXA_COLORS="${EXA_COLORS}:gr=38;5;$COLORS[gray-7]"       # Group read
+  EXA_COLORS="${EXA_COLORS}:gw=38;5;$COLORS[gray-7]"       # Group write
+  EXA_COLORS="${EXA_COLORS}:gx=4;38;5;$COLORS[executable]" # Group execute
 
-  EXA_COLORS="${EXA_COLORS}:tr=38;5;$COLORS[gray-7]"   # Other read
-  EXA_COLORS="${EXA_COLORS}:tw=38;5;$COLORS[gray-7]"   # Other write
-  EXA_COLORS="${EXA_COLORS}:tx=4;38;5;$COLORS[purple]" # Other execute
+  EXA_COLORS="${EXA_COLORS}:tr=38;5;$COLORS[gray-7]"       # Other read
+  EXA_COLORS="${EXA_COLORS}:tw=38;5;$COLORS[gray-7]"       # Other write
+  EXA_COLORS="${EXA_COLORS}:tx=4;38;5;$COLORS[executable]" # Other execute
 
   EXA_COLORS="${EXA_COLORS}:sn=38;5;$COLORS[neutral]"       # Size
   EXA_COLORS="${EXA_COLORS}:sb=38;5;$COLORS[neutral-light]" # Size unit
@@ -35,16 +35,16 @@ function exa-configure-colors() {
   EXA_COLORS="${EXA_COLORS}:gv=38;5;$COLORS[git-modified]"  # File renamed
   EXA_COLORS="${EXA_COLORS}:gt=38;5;$COLORS[variable-type]" # File type changed
 
-  EXA_COLORS="${EXA_COLORS}:di=38;5;$COLORS[green]"      # Directories
-  EXA_COLORS="${EXA_COLORS}:ex=4;38;5;$COLORS[violet-4]" # Executable
+  EXA_COLORS="${EXA_COLORS}:di=38;5;$COLORS[directory]"  # Directories
+  EXA_COLORS="${EXA_COLORS}:ex=38;5;$COLORS[executable]" # Executable
 
-  # EXA_COLORS="${EXA_COLORS}:ln=38;5;69"  # Symlink source
-  # EXA_COLORS="${EXA_COLORS}:lp=38;5;69"  # Symlink destination
-  # EXA_COLORS="${EXA_COLORS}:or=38;5;160" # Broken symlink
-  # EXA_COLORS="${EXA_COLORS}:pi=48;5;13"  # Named pipe (unstyled)
-  # EXA_COLORS="${EXA_COLORS}:cd=48;5;13"  # Character device (unstyled)
-  # EXA_COLORS="${EXA_COLORS}:so=48;5;13"  # Socket (unstyled)
-  # EXA_COLORS="${EXA_COLORS}:bd=48;5;13"  # Block device (unstyled)
+  EXA_COLORS="${EXA_COLORS}:ln=38;5;$COLORS[unknown]" # Symlink source
+  EXA_COLORS="${EXA_COLORS}:lp=38;5;$COLORS[unknown]" # Symlink destination
+  EXA_COLORS="${EXA_COLORS}:or=38;5;$COLORS[unknown]" # Broken symlink
+  EXA_COLORS="${EXA_COLORS}:pi=48;5;$COLORS[unknown]" # Named pipe (unstyled)
+  EXA_COLORS="${EXA_COLORS}:cd=48;5;$COLORS[unknown]" # Character device (unstyled)
+  EXA_COLORS="${EXA_COLORS}:so=48;5;$COLORS[unknown]" # Socket (unstyled)
+  EXA_COLORS="${EXA_COLORS}:bd=48;5;$COLORS[unknown]" # Block device (unstyled)
 
   export EXA_COLORS
 }
--- tools/term/zsh/config/tools/ls.zsh
+++ tools/term/zsh/config/tools/ls.zsh
@@ -20,26 +20,26 @@ function oroshi_tools_ls() {
 	# Define the custom LS_COLORS
 	LS_COLORS_SIMPLE="di=38;5;$COLORS[directory]"      # Directory
 	LS_COLORS_SIMPLE+=":ow=38;5;$COLORS[directory]"    # Directory writable by others
-	LS_COLORS_SIMPLE+=":ex=4;38;5;$COLORS[executable]" # Executable
+	LS_COLORS_SIMPLE+=":ex=38;5;$COLORS[executable]" # Executable
 	LS_COLORS_SIMPLE+=":ln=34;4;$COLORS[link]"         # Symlink
 
 	# Other known LS_COLORS fields, potentially used by the completion styling system
-	# LS_COLORS_SIMPLE+=":st=1;38;5;$COLORS[red]"     # file with sticky bit set
-	# LS_COLORS_SIMPLE+=":pi=1;38;5;$COLORS[green]"   # fifo file
-	# LS_COLORS_SIMPLE+=":mh=1;38;5;$COLORS[green]"   # file with 'b' set (for access control lists)
-	# LS_COLORS_SIMPLE+=":rs=1;38;5;$COLORS[emerald]" # reset to no color code
-	# LS_COLORS_SIMPLE+=":so=1;38;5;$COLORS[yellow]"  # socket file
-	# LS_COLORS_SIMPLE+=":cl=1;38;5;$COLORS[yellow]"  # file with 'c' set (for access control lists)
-	# LS_COLORS_SIMPLE+=":bd=1;38;5;$COLORS[blue]"    # block (buffered) special file
-	# LS_COLORS_SIMPLE+=":tw=1;38;5;$COLORS[blue]"    # directory that is sticky and other-writable (o+w) (+t,+w)
-	# LS_COLORS_SIMPLE+=":st=1;38;5;$COLORS[cyan]"    # directory with the sticky bit set (+t)
-	# LS_COLORS_SIMPLE+=":or=1;38;5;$COLORS[cyan]"    # symbolic link pointing to a non-existent file (orphan)
-	# LS_COLORS_SIMPLE+=":ca=1;38;5;$COLORS[sky]"     # file with capability
-	# LS_COLORS_SIMPLE+=":sg=1;38;5;$COLORS[teal]"    # file with setgid bit set
-	# LS_COLORS_SIMPLE+=":su=1;38;5;$COLORS[violet]"  # file with setuid bit set
-	# LS_COLORS_SIMPLE+=":cd=1;38;5;$COLORS[purple]"  # character (unbuffered) special file
-	# LS_COLORS_SIMPLE+=":mi=1;38;5;$COLORS[orange]"  # non-existent file pointed to by a symbolic link (visible when you type ls -l)
-	# LS_COLORS_SIMPLE+=":tw=1;38;5;$COLORS[amber]"   # sticky other-writable (o+w) file; and sticky other-writable directory
+	LS_COLORS_SIMPLE+=":st=1;38;5;$COLORS[unknown]" # file with sticky bit set
+	LS_COLORS_SIMPLE+=":pi=1;38;5;$COLORS[unknown]" # fifo file
+	LS_COLORS_SIMPLE+=":mh=1;38;5;$COLORS[unknown]" # file with 'b' set (for access control lists)
+	LS_COLORS_SIMPLE+=":rs=1;38;5;$COLORS[unknown]" # reset to no color code
+	LS_COLORS_SIMPLE+=":so=1;38;5;$COLORS[unknown]" # socket file
+	LS_COLORS_SIMPLE+=":cl=1;38;5;$COLORS[unknown]" # file with 'c' set (for access control lists)
+	LS_COLORS_SIMPLE+=":bd=1;38;5;$COLORS[unknown]" # block (buffered) special file
+	LS_COLORS_SIMPLE+=":tw=1;38;5;$COLORS[unknown]" # directory that is sticky and other-writable
+	LS_COLORS_SIMPLE+=":st=1;38;5;$COLORS[unknown]" # directory with the sticky bit set (+t)
+	LS_COLORS_SIMPLE+=":or=1;38;5;$COLORS[unknown]" # orphan symbolic link
+	LS_COLORS_SIMPLE+=":ca=1;38;5;$COLORS[unknown]" # file with capability
+	LS_COLORS_SIMPLE+=":sg=1;38;5;$COLORS[unknown]" # file with setgid bit set
+	LS_COLORS_SIMPLE+=":su=1;38;5;$COLORS[unknown]" # file with setuid bit set
+	LS_COLORS_SIMPLE+=":cd=1;38;5;$COLORS[unknown]" # character (unbuffered) special file
+	LS_COLORS_SIMPLE+=":mi=1;38;5;$COLORS[unknown]" # non-existent file pointed to by a symbolic link
+	LS_COLORS_SIMPLE+=":tw=1;38;5;$COLORS[unknown]" # sticky other-writable (o+w) file
 
 	# Enhance LS_COLORS by looping through all FILETYPES entries that have a :pattern key
 	# Note: It isn't possible to color all hidden files (.*) with LS_COLORS
