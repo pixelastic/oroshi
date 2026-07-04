5db3be2ab1f857b73d8e0195110f33b8ca8e26c0
Tim Carry <tim@pixelastic.com>
2026-07-04 14:55:53 +0200

    refactor(fzf): extract default opts setup into a dedicated lib script
    The FZF_DEFAULT_OPTS configuration is now callable from anywhere in the
    codebase rather than being locked inside the zsh tool init function.

--- tools/term/zsh/config/tools/fzf.zsh
+++ tools/term/zsh/config/tools/fzf.zsh
@@ -5,62 +5,8 @@ function oroshi_tools_fzf() {
   [[ ! -r $fzfPath ]] && return
   source $fzfPath
 
-  colors-load-definitions
-  icons-load-definitions
-
-  # Colors
-  local fzfColors=()
-  fzfColors+=("info:$COLORS[number]")                 # result count
-  fzfColors+=("prompt:${COLORS[ui]}:regular")           # > before query (regular = not bold)
-  fzfColors+=("bg+:$COLORS[selected-background]")     # selected result background
-  fzfColors+=("fg+:$COLORS[selected-foreground]")     # selected result foreground
-  fzfColors+=("pointer:$COLORS[pointer]")             # $ICONS[fzf-current] before current line
-  fzfColors+=("marker:$COLORS[marker]")               # $ICONS[fzf-selected] before selected result
-  fzfColors+=("hl:reverse:$COLORS[match]")            # match
-  fzfColors+=("hl+:reverse:$COLORS[match]")           # match in current line
-  fzfColors+=("gutter:-1")                                # suggestion gutter
-  local fzfOptionColors="--color '${fzfColors:gs/ /,}'"
-
-  # Preview window
-  # If the preview on the right get smaller than that number of chars, we pass
-  # it in horizontal mode. Note that this number will kinda depend on the
-  # font size used.
-  local maxCharWidth=72
-  local fzfOptionPreview="\
-    --preview-window 'right,50%,border-left,<${maxCharWidth}(bottom,50%,border-top)' \
-    --bind 'ctrl-j:preview-down' \
-    --bind 'ctrl-k:preview-up' \
-  "
-  # Multi-select options
-  local fzfOptionsMulti="\
-    --multi \
-    --marker="$ICONS[fzf-selected]" \
-    --bind 'enter:accept' \
-    --bind 'ctrl-space:toggle+down' \
-    --bind 'ctrl-a:toggle-all' \
-  "
-
-  # Generic options
-  local fzfOptionBase="\
-    --ansi \
-    --height=100% \
-    --delimiter=▮ \
-    --pointer=$ICONS[fzf-current] \
-    --reverse \
-    --cycle \
-    --keep-right \
-    --no-hscroll \
-    --bind 'tab:down' \
-    --bind 'shift-up:half-page-up' \
-    --bind 'shift-down:half-page-down' \
-    --bind 'home:first' \
-    --bind 'end:last' \
-    --bind 'ctrl-l:clear-query+first' \
-    --bind 'bspace:backward-delete-char+first' \
-  "
-
-  # Default config
-  export FZF_DEFAULT_OPTS="$fzfOptionBase $fzfOptionsMulti $fzfOptionColors $fzfOptionPreview"
+  source "$OROSHI_ROOT/scripts/bin/fzf/__lib/fzf-set-default-opts.zsh"
+  fzf-set-default-opts
 }
 oroshi_tools_fzf
 unfunction oroshi_tools_fzf
