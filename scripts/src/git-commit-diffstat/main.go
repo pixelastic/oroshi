e9da7bb7faecb22f7722d112474f2b1ff49647f2
Tim Carry <tim@pixelastic.com>
2026-09-13 21:49:13 +0200

    feat(git-commit-diffstat): render full diffstat with tree and summary
    The tool now displays a file tree with per-file change bars and a
    summary line instead of a placeholder print statement. Commit diffs are
    fully visualized end-to-end.

--- scripts/src/git-commit-diffstat/main.go
+++ scripts/src/git-commit-diffstat/main.go
@@ -3,8 +3,14 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
+	"strings"
 
+	"github.com/pixelastic/oroshi/scripts/src/git-commit-diffstat/bar"
+	"github.com/pixelastic/oroshi/scripts/src/git-commit-diffstat/diff"
+	"github.com/pixelastic/oroshi/scripts/src/git-commit-diffstat/summary"
 	"github.com/pixelastic/oroshi/scripts/src/git-commit-diffstat/theme"
+	"github.com/pixelastic/oroshi/scripts/src/git-commit-diffstat/tree"
 )
 
 func main() {
@@ -14,17 +20,67 @@ func main() {
 		os.Exit(1)
 	}
 
+	from, to, repo := args[0], args[1], args[2]
+
 	oroshiRoot := os.Getenv("OROSHI_ROOT")
 	if oroshiRoot == "" {
 		fmt.Fprintln(os.Stderr, "OROSHI_ROOT not set")
 		os.Exit(1)
 	}
 
-	_, err := theme.Load(oroshiRoot)
+	thm, err := theme.Load(oroshiRoot)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 
-	fmt.Printf("git-commit-diffstat: from=%s to=%s repo=%s\n", args[0], args[1], args[2])
+	changes, err := diff.Query(from, to, repo, execRunner)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	if len(changes) == 0 {
+		return
+	}
+
+	barColors := bar.Colors{
+		Added:    thm.Lipgloss("git-added"),
+		Removed:  thm.Lipgloss("git-removed"),
+		Modified: thm.Lipgloss("git-modified"),
+	}
+	barRenderer := func(change diff.FileChange) string {
+		return bar.Render(change.Additions, change.Deletions, change.Status, change.IsBinary, barColors)
+	}
+
+	t := tree.Build(changes)
+	fmt.Print(tree.Render(t, thm, barRenderer))
+
+	var created, deleted int
+	for _, c := range changes {
+		switch c.Status {
+		case diff.Added:
+			created++
+		case diff.Deleted:
+			deleted++
+		}
+	}
+
+	fileIcon, _ := thm.Icon("filetype-file")
+	summaryLine := summary.Render(len(changes), created, deleted, summary.Theme{
+		FileIcon:     fileIcon,
+		CreatedColor: thm.Lipgloss("git-added"),
+		DeletedColor: thm.Lipgloss("git-removed"),
+	})
+	if summaryLine != "" {
+		fmt.Println(summaryLine)
+	}
+}
+
+func execRunner(name string, args ...string) (string, error) {
+	out, err := exec.Command(name, args...).Output()
+	if err != nil {
+		return "", err
+	}
+	return strings.TrimRight(string(out), "\n"), nil
 }
